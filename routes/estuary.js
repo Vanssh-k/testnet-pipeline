ff3b0195b1ff7d6f1e95e43b19179b4c60259d4e
ravish1729 <ravish1729@gmail.com>
2021-11-19 04:51:12 +0530

    estuary api integrated

--- routes/estuary.js
+++ routes/estuary.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const EstuaryController = require('../controller/estuary/EstuaryController')
 
 router.get("/user_token", EstuaryController.user_token);
+router.get("/metadata_by_cid", EstuaryController.metadata_by_cid);
+router.get("/list_data", EstuaryController.list_data);
+router.get("/get_deals_filecoin", EstuaryController.get_deals_filecoin);
+router.get("/get_deals", EstuaryController.get_deals);
 
 module.exports = router;
