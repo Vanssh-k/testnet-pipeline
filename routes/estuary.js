4cf6fc52aae09b5d853bfac87831096e34d2bdfa
ravish1729 <ravish1729@gmail.com>
2022-01-05 04:28:23 +0530

    updated routes

--- routes/estuary.js
+++ routes/estuary.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const EstuaryController = require("../controller/estuary/EstuaryController");
 
-router.get("/user_token", EstuaryController.user_token);
+router.post("/user_token", EstuaryController.user_token);
 router.get("/status/:cid", EstuaryController.status);
 router.get("/list_data", EstuaryController.list_data);
 router.get("/get_deals_filecoin", EstuaryController.get_deals_filecoin);
 router.get("/get_deals", EstuaryController.get_deals);
 router.post("/get_quote", EstuaryController.get_quote);
 router.post("/push_cid_tochain", EstuaryController.push_cid_tochain);
+router.get("/check_address", EstuaryController.check_address);
 
 module.exports = router;
