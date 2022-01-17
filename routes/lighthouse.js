72be121b13308c44eba38f44da20e0d3710e5baf
ravish1729 <ravish1729@gmail.com>
2022-01-17 19:09:51 +0530

    added infura endpoint

--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const EstuaryController = require("../controller/estuary/EstuaryController");
+const InfuraController = require("../controller//Infura");
 
 router.post("/user_token", EstuaryController.user_token);
 router.get("/status/:cid", EstuaryController.status);
@@ -8,5 +9,7 @@ router.get("/list_data", EstuaryController.list_data);
 router.get("/get_deals", EstuaryController.get_deals);
 router.post("/get_quote", EstuaryController.get_quote);
 router.get("/get_deals_filecoin", EstuaryController.get_deals_filecoin);
+
+router.get("/upload_client", InfuraController.upload_client);
 
 module.exports = router;
