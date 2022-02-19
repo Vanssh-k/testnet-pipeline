eb2f788a9498b986e5e6da1de647f92c6f52bc71
ravish1729 <ravish1729@gmail.com>
2022-02-20 02:20:57 +0530

    authentication endpoints

--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const InfuraController = require("../controller/Infura");
 const EstuaryController = require("../controller/estuary");
 const ContractController = require("../controller/contract");
+const AuthController = require("../controller/authentication");
 
 router.post("/user_token", EstuaryController.user_token);
 router.get("/get_ticker", EstuaryController.get_ticker);
@@ -16,5 +17,8 @@ router.get("/get_deals_filecoin", EstuaryController.get_deals_filecoin);
 
 router.get("/get_uploads", ContractController.get_uploads);
 router.get("/upload_client", InfuraController.upload_client);
+
+router.get("/verify_signer", AuthController.verify_signer);
+router.get("/get_message", AuthController.get_message);
 
 module.exports = router;
