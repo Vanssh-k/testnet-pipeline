5d77a4c167299ef95f8150d9104a94a4016f4856
ravish1729 <ravish1729@gmail.com>
2022-03-18 03:24:41 +0530

    added data parameters in db

--- controller/authentication/index.js
+++ controller/authentication/index.js
@@ -2,7 +2,7 @@ const AWS = require("aws-sdk");
 const ethers = require("ethers");
 const { v4: uuidv4 } = require("uuid");
 
-const tableName = "UserAuth";
+const tableName = "user";
 AWS.config.update({
   aws_table_name: tableName,
   accessKeyId: process.env.aws_access_key_id,
@@ -55,15 +55,40 @@ exports.verify_signer = async (req, res) => {
   }
 };
 
+const returning_user = async (publicKey) => {
+  const params = {
+    TableName: tableName,
+    FilterExpression: "publicKey = :p",
+    ExpressionAttributeValues: {
+      ":p": publicKey,
+    },
+  };
+
+  return new Promise(function (resolve, reject) {
+    client.scan(params, function (err, data) {
+      if (err) {
+        reject(false);
+      } else {
+        const { Items } = data;
+        resolve(Items[0]);
+      }
+    });
+  });
+};
+
 exports.get_message = async (req, res) => {
   try {
+    const publicKey = req.query.publicKey.toLowerCase();
+    const record = await returning_user(publicKey);
     const message = uuidv4();
+
     const params = {
       TableName: tableName,
       Item: {
-        ID: message,
-        publicKey: req.query.publicKey,
+        publicKey: publicKey,
         message: message.toString(),
+        dataLimit: record ? record.dataLimit : 1,
+        dataUsed: record ? record.dataUsed : 0,
       },
     };
 
@@ -82,3 +107,75 @@ exports.get_message = async (req, res) => {
     });
   }
 };
+
+// exports.get_message = async (req, res) => {
+//   try {
+//     const message = uuidv4();
+//     const params = {
+//       TableName: tableName,
+//       Item: {
+//         ID: message,
+//         publicKey: req.query.publicKey,
+//         message: message.toString(),
+//       },
+//     };
+
+//     client.put(params, function (err, data) {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).json("Internal Server Error");
+//       } else {
+//         console.log("PutItem succeeded:");
+//         res.status(200).json(message);
+//       }
+//     });
+//   } catch (e) {
+//     res.status(500).send({
+//       message: "Internal Server Error",
+//     });
+//   }
+// };
+
+// exports.verify_signer = async (req, res) => {
+//   try {
+//     const params = {
+//       TableName: tableName,
+//       FilterExpression: "publicKey = :p",
+//       ExpressionAttributeValues: {
+//         ":p": req.query.publicKey,
+//       },
+//     };
+
+//     client.scan(params, function (err, data) {
+//       if (err) {
+//         res.status(500).send({
+//           message: "Internal Server Error",
+//         });
+//       } else {
+//         const { Items } = data;
+//         if (Items.length > 0) {
+//           try {
+//             const sig = ethers.utils.splitSignature(req.query.signed_message);
+//             const publicKey = ethers.utils.verifyMessage(
+//               Items[0]["message"],
+//               sig
+//             );
+//             if (req.query.publicKey === publicKey) {
+//               res.status(200).json("Authorized");
+//             } else {
+//               res.status(401).json("UnAuthorized");
+//             }
+//           } catch {
+//             res.status(401).json("UnAuthorized");
+//           }
+//         } else {
+//           res.status(401).json("UnAuthorized");
+//         }
+//       }
+//     });
+//   } catch (e) {
+//     res.status(500).send({
+//       message: "Internal Server Error",
+//     });
+//   }
+// };
