912ce3825811b9a3183660f97953eea78bd71bdc
ravish1729 <ravish1729@gmail.com>
2022-04-08 11:55:09 +0530

    user and payment verification to add subdomain

--- controller/subdomain/index.js
+++ controller/subdomain/index.js
@@ -1,6 +1,11 @@
 const AWS = require("aws-sdk");
+const { default: axios } = require("axios");
+
+const lighthouseConfig = require("../../lighthouse.config");
+const {verify_signer, verify_api_key} = require("./verify");
 
 const subDomainTable = "SubDomainManagement";
+
 
 AWS.config.update({
   aws_table_name: subDomainTable,
@@ -12,27 +17,53 @@ const client = new AWS.DynamoDB.DocumentClient();
 
 exports.add_subdomain = async (req, res) => {
   try {
-    const timestamp = Date.now();
-    const params = {
-      TableName: subDomainTable,
-      Item: {
-        publicKey: req.body.publicKey.toLowerCase(),
-        subDomain: req.body.subDomain,
-        txHash: "",
-        createdAt: timestamp,
-        lastUpdate: timestamp,
-      },
-    };
+    const network = req.body.network;
+    const txHash = req.body.txHash;
+    const chainId = lighthouseConfig[network]["chain_id"];
 
-    client.put(params, function (err, data) {
-      if (err) {
-        console.error(err);
-        res.status(500).json("Internal Server Error");
-      } else {
-        console.log("PutItem succeeded:");
-        res.status(200).json("PutItem succeeded:");
+    let verified = false;
+    if(req.body.signed_message){
+      verified = await verify_signer(req.body.publicKey.toLowerCase(), req.body.signed_message);
+    }else{
+      verified = await verify_api_key(req.body.apiKey);
+    }
+
+    console.log("here")
+    if(verified){
+      console.log("here2")
+      const txReceipt = (await axios.get("https://deep-index.moralis.io/api/v2/transaction/0x1cced4ed634aa2a4d4fc539f3c15c474b64c6194f50d757339b38cb0cfb817eb?chain=fantom",{ headers: { "x-api-key": process.env.moralisAPIKey }})).data
+      console.log(txReceipt)
+      const value = txReceipt["value"];
+      const from = txReceipt.from_address.toLocaleLowerCase();
+      const to = txReceipt.to_address.toLowerCase();
+      console.log(value, to, from)
+      if(value && to==="0xf468602B34C482f34ca498D9a0DE7957539961d3".toLocaleLowerCase() && from===req.body.publicKey.toLocaleLowerCase()){
+        const timestamp = Date.now();
+        const params = {
+          TableName: subDomainTable,
+          Item: {
+            publicKey: req.body.publicKey.toLowerCase(),
+            subDomain: req.body.subDomain,
+            txHash: "",
+            createdAt: timestamp,
+            lastUpdate: timestamp,
+          },
+        };
+
+        client.put(params, function (err, data) {
+          if (err) {
+            console.error(err);
+            res.status(500).json("Internal Server Error");
+          } else {
+            console.log("PutItem succeeded:");
+            res.status(200).json("PutItem succeeded:");
+          }
+        });
+      } else{
+        res.status(500).json("Value error");
       }
-    });
+    }
+    
   } catch (e) {
     console.log(e);
     res.status(500).send({
