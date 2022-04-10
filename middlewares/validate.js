610824686404b46fa81c495f5d10ca9b9436bdc6
ravish <ravish1729@gmail.com>
2022-04-10 23:01:58 +0530

    validation error middleware

---
+++ middlewares/validate.js
@@ -0,0 +1,12 @@
+const { validationResult } = require('express-validator');
+
+module.exports = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        let error = {};
+        errors.array().map((err) => error[err.param] = err.msg);
+        console.error(error);
+        return res.status(422).json({error});
+    }
+    next();
+};
