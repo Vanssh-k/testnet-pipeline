7ececca6dd613bcb7c8a9aa32ae3a0e418d555ae
ravish <ravish1729@gmail.com>
2022-04-10 23:01:04 +0530

    added auth route

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const AuthController = require("../controller/authentication");
+const { body, query } = require("express-validator");
+
+const router = express.Router();
+const validate = require('../middlewares/validate');
+
+router.post("/verify_signer", [
+  body("publicKey").trim().not().isEmpty().withMessage("publicKey not found"),
+  body("signedMessage").trim().not().isEmpty().withMessage("signedMessage not found"),
+], validate, AuthController.verify_signer);
+
+router.post("/verify_signer_with_data", [
+  body("publicKey").trim().not().isEmpty().withMessage("publicKey not found"),
+  body("signedMessage").trim().not().isEmpty().withMessage("signedMessage not found"),
+], validate, AuthController.verify_signer_with_data);
+
+router.get("/get_message", [
+  query('publicKey').not().isEmpty().withMessage("publicKey not found"),
+], validate, AuthController.get_message);
+
+router.post("/get_api_key", [
+  body("publicKey").trim().not().isEmpty().withMessage("publicKey not found"),
+  body("signedMessage").trim().not().isEmpty().withMessage("signedMessage not found"),
+], validate, AuthController.get_api_key);
+
+router.get("/verify_api_key", [
+  query('apiKey').not().isEmpty().withMessage("apiKey not found"),
+], validate, AuthController.verify_api_key);
+
+module.exports = router;
