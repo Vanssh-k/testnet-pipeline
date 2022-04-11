6b648d5c2ce1d241d9cf70a8518b2a08916c39c1
ravish <ravish1729@gmail.com>
2022-04-11 11:46:51 +0530

    get transaction details method

---
+++ controller/gateway/getTransactionDetails.js
@@ -0,0 +1,27 @@
+const dbbClient = require("../libs/ddbClient");
+const { gatewayTable } = require("../libs/constants");
+
+module.exports = async (usersPublicKey) =>{
+  const params = {
+    TableName: gatewayTable,
+    FilterExpression: "publicKey = :p",
+    ExpressionAttributeValues: {
+      ":p": usersPublicKey,
+    },
+  };
+
+  return new Promise(function (resolve, reject) {
+    dbbClient.scan(params, function (err, data) {
+      try{
+        if (err) {
+          reject(err);
+        }
+        const { Items } = data;
+        resolve(Items[0]);
+      } catch (error){
+        console.error(error);
+        reject(null);
+      }
+    });
+  });
+};
