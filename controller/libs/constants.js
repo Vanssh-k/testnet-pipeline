ed6f0bbd5ee545ab85752525f7f1c2c51f4c6459
ravish <ravish1729@gmail.com>
2022-04-11 11:35:12 +0530

    constants

---
+++ controller/libs/constants.js
@@ -0,0 +1,6 @@
+const userTable = "Users";
+const fileTable = "FileManagement";
+const gatewayTable = "SubDomainManagement";
+const freeDataLimitInBytes = 1073741824;
+
+module.exports = {userTable, fileTable, freeDataLimitInBytes, gatewayTable}
