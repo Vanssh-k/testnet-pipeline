b7b69b9932f54616819ef4bc94492bd52986e854
ravish <ravish1729@gmail.com>
2022-04-11 12:55:11 +0530

    method to save file details in db

---
+++ controller/lighthouse/saveFileMetaData.js
@@ -0,0 +1,40 @@
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+
+const fileTable = "FileManagement";
+AWS.config.update({
+  aws_table_name: fileTable,
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+  region: "ap-south-1",
+});
+const client = new AWS.DynamoDB.DocumentClient();
+
+module.exports = async (publicKey, cid, fileName, fileSizeInBytes, status) => {
+  const id = uuidv4();
+  const timestamp = Date.now();
+  const params = {
+    TableName: fileTable,
+    Item: {
+      id: id,
+      publicKey: publicKey,
+      cid: cid,
+      fileName: fileName,
+      fileSizeInBytes: fileSizeInBytes,
+      status: status,
+      txHash: "",
+      createdAt: timestamp,
+      lastUpdate: timestamp,
+    },
+  };
+
+  client.put(params, function (err, data) {
+    if (err) {
+      console.error(err);
+      return "error";
+    } else {
+      console.log("PutItem succeeded:");
+      return "updated";
+    }
+  });
+};
