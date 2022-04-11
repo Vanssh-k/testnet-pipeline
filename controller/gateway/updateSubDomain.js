ebebe6a6ece83f1fdd9ee88f5a7dab07529c081b
ravish <ravish1729@gmail.com>
2022-04-11 11:47:31 +0530

    method to update gateway subDomain

---
+++ controller/gateway/updateSubDomain.js
@@ -0,0 +1,21 @@
+const dbbClient = require("../libs/ddbClient");
+const { gatewayTable } = require("../libs/constants");
+
+module.exports = async (transactionDetails) =>{
+  const params = {
+    TableName: gatewayTable,
+    Item: transactionDetails,
+  };
+
+  return new Promise(function (resolve, reject) {
+    dbbClient.put(params, function (err, data) {
+      if (err) {
+        console.log(err);
+        reject(false);
+      } else {
+        console.log("PutItem succeeded:");
+        resolve(true);
+      }
+    });
+  });
+};
