139e4538d329388a9632eb67083afec8476f2868
ravish <ravish1729@gmail.com>
2022-04-15 21:45:36 +0530

    updated with error handling

--- controller/lighthouse/index.js
+++ controller/lighthouse/index.js
@@ -3,112 +3,91 @@ const updateUserDetails = require("../authentication/updateUserDetails");
 const userDetails = require("../authentication/userDetails");
 const saveFileMetaData = require("./saveFileMetaData");
 
-// get ticker of a token by its symbol as input
-exports.get_ticker = async (req, res) => {
-  try {
-    const token_prices = await axios.get(
-      `https://data.messari.io/api/v1/assets/${req.query.symbol}/metrics/market-data`
-    );
+const ForbiddenError = require("../../errors/forbidden");
+const DatabaseError = require("../../errors/database-error");
 
-    const token_price_usd = token_prices.data.data.market_data.price_usd;
+// get ticker of a token by its symbol as input
+exports.get_ticker = async (req, res, next) => {
+  try {
+    const token_prices = (
+      await axios.get(
+        `https://data.messari.io/api/v1/assets/${req.query.symbol}/metrics/market-data`
+      )
+    ).data;
+
+    const token_price_usd = token_prices.data.market_data.price_usd;
     res.status(200).json(token_price_usd);
-  } catch {
-    try {
-      const token_prices = await axios.get(
-        `https://api.covalenthq.com/v1/pricing/tickers/?quote-currency=USD&format=JSON&page-size=1&tickers=${req.query.symbol}&key=${process.env.COVALENT_API_KEY}`
-      );
-      res.status(200).json(token_prices.data.data["items"][0]["quote_rate"]);
-    } catch (e) {
-      res.status(500).send({
-        message: "Internal Server Error",
-      });
-    }
+  } catch (error) {
+    next(error);
   }
 };
 
 // get status of a CID, returns filecoin miner details
-exports.cid_status = async (req, res) => {
+exports.cid_status = async (req, res, next) => {
   try {
     const headers = {
       Authorization: `Bearer ${process.env.EST_API_KEY}`,
       Accept: "application/json",
     };
 
-    const response = await axios.get(
-      `https://api.estuary.tech/content/by-cid/${req.query.cid}`,
-      { headers: headers }
-    );
-    res.status(200).json(response.data);
-  } catch (e) {
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+    const response = (
+      await axios.get(
+        `https://api.estuary.tech/content/by-cid/${req.query.cid}`,
+        { headers: headers }
+      )
+    ).data;
+
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
   }
 };
 
-const addCid = async (name, cid) =>{
-  const headers = {
-    Authorization: `Bearer ${process.env.EST_API_KEY}`,
-    Accept: "application/json",
-  };
-
-  const response = await axios.post(
-    `https://api.estuary.tech/content/add-ipfs`,
-    {
-      name: name,
-      root: cid,
-    },
-    { headers: headers }
-  );
-
-  return response.data;
-}
-// add cid for filecoin deal
-exports.add_cid = async (req, res) => {
+const addCid = async (name, cid) => {
   try {
-    const response = await addCid(req.body.name, req.body.cid)
-    res.status(200).json(response);
-  } catch (e) {
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+    const headers = {
+      Authorization: `Bearer ${process.env.EST_API_KEY}`,
+      Accept: "application/json",
+    };
+
+    const response = (
+      await axios.post(
+        `https://api.estuary.tech/content/add-ipfs`,
+        {
+          name: name,
+          root: cid,
+        },
+        { headers: headers }
+      )
+    ).data;
+
+    return response;
+  } catch (error) {
+    return null;
+  }
+};
+
+// add cid for filecoin deal
+exports.add_cid = async (req, res, next) => {
+  try {
+    const addCidResponse = await addCid(req.body.name, req.body.cid);
+    if (addCidResponse === null) {
+      throw new DatabaseError("Add CID Failed");
+    }
+
+    res.status(200).json("Added To Queue");
+  } catch (error) {
+    next(error);
   }
 };
 
 // Add file to queue for bundled transaction
-exports.add_cid_to_queue = async (req, res) => {
+exports.add_cid_to_queue = async (req, res, next) => {
   try {
     const publicKey = req.body.publicKey.toLowerCase();
-    const record = await userDetails(publicKey);  // Get record of user
+    const record = await userDetails(publicKey); // Get record of user
 
-    if (req.body.size <= record.dataLimit - record.dataUsed) {
-      // Create record of file
-      await saveFileMetaData(
-        publicKey,
-        req.body.cid,
-        req.body.name,
-        req.body.size,
-        "queued"
-      );
-
-      // Update data usage
-      const updatedDetails = {
-        publicKey: publicKey,
-        message: record.message,
-        dataLimit: record.dataLimit,
-        dataUsed: parseInt(record.dataUsed) + parseInt(req.body.size),
-        apiKey: record.apiKey,
-      };
-      const updateResponse = await updateUserDetails(updatedDetails);
-
-      // Send CID to Estuary
-      const addCidResponse = await addCid(
-        req.body.name,
-        req.body.cid
-      );
-
-      res.status(200).json(addCidResponse);
-    } else {
+    if (req.body.size > record.dataLimit - record.dataUsed) {
       // Create record of file
       await saveFileMetaData(
         publicKey,
@@ -117,12 +96,45 @@ exports.add_cid_to_queue = async (req, res) => {
         req.body.size,
         "payment pending"
       );
-      res.status(500).json("Uploaded more than allowed limit");
+
+      throw new ForbiddenError();
     }
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+
+    // Create record of file
+    const saveFileResponse = await saveFileMetaData(
+      publicKey,
+      req.body.cid,
+      req.body.name,
+      req.body.size,
+      "queued"
+    );
+    if (saveFileResponse === null) {
+      throw new DatabaseError("Save File failed");
+    }
+
+    // Update data usage
+    const updatedDetails = {
+      publicKey: publicKey,
+      message: record.message,
+      dataLimit: record.dataLimit,
+      dataUsed: parseInt(record.dataUsed) + parseInt(req.body.size),
+      apiKey: record.apiKey,
+    };
+
+    const updateResponse = await updateUserDetails(updatedDetails);
+    if (updateResponse === null) {
+      throw new DatabaseError("Put item failed");
+    }
+
+    // Send CID to Estuary
+    const addCidResponse = await addCid(req.body.name, req.body.cid);
+
+    if (addCidResponse === null) {
+      throw new DatabaseError("Add CID Failed");
+    }
+
+    res.status(200).json("Success");
+  } catch (error) {
+    next(error);
   }
 };
