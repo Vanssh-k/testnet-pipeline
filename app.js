a69de61b736c175deaf5237e131e2e59ed9ca3f9
ravish <ravish1729@gmail.com>
2022-04-15 21:43:14 +0530

    lint

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const errorHandler = require('./middlewares/error-handler');
+const errorHandler = require("./middlewares/error-handler");
 require("dotenv").config();
 
 global.__basedir = __dirname;
