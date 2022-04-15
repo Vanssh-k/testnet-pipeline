ee5cccd2fcbc0b6db2614c7ed7876804f9ea69f8
ravish <ravish1729@gmail.com>
2022-04-15 17:34:24 +0530

    better error handling

--- controller/authentication/index.js
+++ controller/authentication/index.js
@@ -7,8 +7,12 @@ const verifySignature = require("./verifySignature");
 const updateUserDetails = require("./updateUserDetails");
 const { freeDataLimitInBytes } = require("../libs/constants");
 
+const DatabaseError = require("../../errors/database-error");
+const AuthenticationError = require("../../errors/authentication-error");
+const NotFoundError = require("../../errors/not-found-error");
+
 // Return if user is authentic
-exports.verify_signer = async (req, res) => {
+exports.verify_signer = async (req, res, next) => {
   try {
     const usersPublicKey = req.body.publicKey;
     const signedMessage = req.body.signedMessage;
@@ -19,20 +23,17 @@ exports.verify_signer = async (req, res) => {
       signedMessage
     );
 
-    if(authentic){
-      res.status(200).json("Authorized");
-    } else {
-      res.status(401).json("UnAuthorized");
+    if(!authentic){
+      throw new AuthenticationError();
     }
-  } catch (e) {
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+    res.status(200).json("Authorized");
+  } catch (error) {
+    next(error);
   }
 };
 
 // Return if user is authentic along with his data usage
-exports.verify_signer_with_data = async (req, res) => {
+exports.verify_signer_with_data = async (req, res, next) => {
   try {
     const usersPublicKey = req.body.publicKey;
     const signedMessage = req.body.signedMessage;
@@ -44,23 +45,20 @@ exports.verify_signer_with_data = async (req, res) => {
       signedMessage
     );
 
-    if(authentic){
-      res.status(200).json({
-        dataLimit: user.dataLimit,
-        dataUsed: user.dataUsed,
-      })
-    } else {
-      res.status(401).json("UnAuthorized");
+    if(!authentic){
+      throw new AuthenticationError();
     }
-  } catch (e) {
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+    res.status(200).json({
+      dataLimit: user.dataLimit,
+      dataUsed: user.dataUsed,
+    })
+  } catch (error) {
+    next(error);
   }
 };
 
 // Get message - user will sign this message to verify himself
-exports.get_message = async (req, res) => {
+exports.get_message = async (req, res, next) => {
   try {
     const publicKey = req.query.publicKey;
     const record = await userDetails(publicKey);        // Check if user already exist
@@ -75,63 +73,65 @@ exports.get_message = async (req, res) => {
     };
     
     const updateResponse = await updateUserDetails(updatedDetails);
+    if(updateResponse===null){
+      throw new DatabaseError("Put item failed");
+    }
+
     res.status(200).json(message);
-  } catch (e) {
-    res.status(500).send({
-      message: "Internal Server Error",
-    });
+  } catch (error) {
+    next(error);
   }
 };
 
-exports.get_api_key = async (req, res) => {
-  const usersPublicKey = req.body.publicKey;
-  const signedMessage = req.body.signedMessage;
-  const record = await userDetails(usersPublicKey);
-  if (record) {
+exports.get_api_key = async (req, res, next) => {
+  try{
+    const usersPublicKey = req.body.publicKey;
+    const signedMessage = req.body.signedMessage;
+    const record = await userDetails(usersPublicKey);
+
+    if(record===null){
+      throw new NotFoundError();
+    }
+    
     const authentic = verifySignature(
       usersPublicKey,
       record["message"],
       signedMessage
     );
-    
-    if (authentic) {
-      const apiKey = uuidv4().toString();
-      const updatedDetails = {
-        publicKey: record.publicKey,
-        message: record.message,
-        dataLimit: record.dataLimit,
-        dataUsed: record.dataUsed,
-        apiKey: SHA256(apiKey).toString(),
-      };
 
-      const updateResponse = await updateUserDetails(updatedDetails);
-      if(updateResponse==="Update Successful"){
-        res.status(200).json(apiKey);
-      } else{
-        res.status(500).json("Internal Server Error!!!");
-      }
-    } else {
-      res.status(401).json("UnAuthorized");
+    if(!authentic){
+      throw new AuthenticationError();
     }
-  } else {
-    res.status(401).json("UnAuthorized");
+    
+    const apiKey = uuidv4().toString();
+    const updatedDetails = {
+      publicKey: record.publicKey,
+      message: record.message,
+      dataLimit: record.dataLimit,
+      dataUsed: record.dataUsed,
+      apiKey: SHA256(apiKey).toString(),
+    };
+    
+    const updateResponse = await updateUserDetails(updatedDetails);
+    if(updateResponse===null){
+      throw new DatabaseError("Put item failed");
+    }
+
+    res.status(200).json(apiKey);
+  } catch(error){
+    next(error);
   }
 };
 
 exports.verify_api_key = async (req, res) => {
-  try{
-    const apiKey = req.headers["authorization"].split(" ")[1];
-    const record = await checkApiKey(SHA256(apiKey).toString());
-    if (record) {
-      res.status(200).json({
-        publicKey: record.publicKey,
-        dataLimit: record.dataLimit,
-        dataUsed: record.dataUsed,
-      });
-    } else {
-      res.status(401).json("UnAuthorized");
-    }
-  } catch{
+  const record = await checkApiKey(SHA256(req.query.apiKey).toString());
+  if (record) {
+    res.status(200).json({
+      publicKey: record.publicKey,
+      dataLimit: record.dataLimit,
+      dataUsed: record.dataUsed,
+    });
+  } else {
     res.status(401).json("UnAuthorized");
   }
 };
