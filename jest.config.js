68da678c6ab2c456a35fdc8470956df1f791f8f3
ravish <ravish1729@gmail.com>
2022-04-15 17:35:21 +0530

    jest config

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const coverageToNumber = 90; // [0..100]
+
+// Or async function
+module.exports = async () => {
+  return {
+    verbose: true,
+    rootDir: "./",
+    clearMocks: true, // clear mocks before every test
+    resetMocks: false, // reset mock state before every test
+    testMatch: ["<rootDir>/**/*.test.js"], // match only tests inside /tests folder
+    testPathIgnorePatterns: [
+      "<rootDir>/node_modules/",
+      "<rootDir>/.trunk/",
+    ], // exclude unnecessary folders
+
+    // following lines are about coverage
+    collectCoverage: true, //[true|false]
+    collectCoverageFrom: ["<rootDir>/**/*.js"],
+    coverageDirectory: "<rootDir>/tests/coverage",
+    coverageReporters: ["lcov"],
+    coverageThreshold: {
+      global: {
+        branches: coverageToNumber,
+        functions: coverageToNumber,
+        lines: coverageToNumber,
+        statements: coverageToNumber,
+      },
+    },
+  };
+};
