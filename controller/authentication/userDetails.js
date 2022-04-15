36246e64c44e8c0093a69b1ae3832502907a69ef
ravish <ravish1729@gmail.com>
2022-04-15 17:33:51 +0530

    imporved code

--- controller/authentication/userDetails.js
+++ controller/authentication/userDetails.js
@@ -2,26 +2,19 @@ const dbbClient = require("../libs/ddbClient");
 const { userTable } = require("../libs/constants");
 
 module.exports = async(usersPublicKey) =>{
-  const params = {
-    TableName: userTable,
-    FilterExpression: "publicKey = :p",
-    ExpressionAttributeValues: {
-      ":p": usersPublicKey.trim().toLowerCase(),
-    },
-  };
+  try{
+    const params = {
+      TableName: userTable,
+      FilterExpression: "publicKey = :p",
+      ExpressionAttributeValues: {
+        ":p": usersPublicKey.trim().toLowerCase(),
+      },
+    };
 
-  return new Promise(function (resolve, reject) {
-    dbbClient.scan(params, function (err, data) {
-      try{
-        if (err) {
-          reject(err);
-        }
-        const { Items } = data;
-        resolve(Items[0]);
-      } catch (error){
-        console.error(error);
-        reject(null);
-      }
-    });
-  })
-};
+    const record = await dbbClient.scan(params).promise();
+    const { Items } = record;
+    return(Items[0]);
+  } catch (error){
+    return(null);
+  }
+}
