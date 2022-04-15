cd74e0a0a93a5f02af45ec51880087e69878e8e6
ravish <ravish1729@gmail.com>
2022-04-15 10:31:58 +0530

    updated with error classes

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,12 +1,17 @@
 const { validationResult } = require('express-validator');
+const RequestValidationError = require("../errors/request-validation-error");
 
-module.exports = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        let error = {};
-        errors.array().map((err) => error[err.param] = err.msg);
-        console.error(error);
-        return res.status(422).json({error});
+module.exports = (req, res, next) => { 
+    try{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            let error = {};
+            errors.array().map((err) => error[err.param] = err.msg);
+            console.error(error);
+            throw new RequestValidationError(errors.array());
+        }
+        next();
+    } catch(error){
+        next(error);
     }
-    next();
 };
