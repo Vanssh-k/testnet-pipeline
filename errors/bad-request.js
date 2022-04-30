e344b986856603d6d4468fdbf65c94ab99bd4860
ravish <ravish1729@gmail.com>
2022-04-30 16:46:25 +0530

    bad request error

---
+++ errors/bad-request.js
@@ -0,0 +1,12 @@
+const CustomError = require("./custom-error");
+
+module.exports = class BadRequestError extends CustomError {
+  constructor() {
+    super("Bad Request");
+    this.statusCode = 400;
+    Object.setPrototypeOf(this, BadRequestError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: "Bad Request" }];
+  }
+};
