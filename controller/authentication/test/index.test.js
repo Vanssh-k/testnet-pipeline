b6edcaf5b728b45534bffef4a1eb9684c01df29f
ravish <ravish1729@gmail.com>
2022-04-30 16:41:30 +0530

    updated test cases

--- controller/authentication/test/index.test.js
+++ controller/authentication/test/index.test.js
@@ -24,7 +24,7 @@ test("Verify Signer: POST /verify_signer", async () => {
       const verificationMessage = JSON.parse(response.text);
       const provider = new ethers.getDefaultProvider();
       const signer = new ethers.Wallet(
-        process.env.TEST_WALLET_PRIVATE_KEY,
+        process.env.TEST_WALLET1_PRIVATE_KEY,
         provider
       );
       const signedMessage = await signer.signMessage(verificationMessage);
@@ -44,6 +44,18 @@ test("Verify Signer: POST /verify_signer", async () => {
     });
 }, 10000);
 
+test("Verify Signer Unauthorized Case: POST /verify_signer", async () => {
+  const data = {
+    publicKey: "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1",
+    signedMessage: "signedMessage",
+  };
+
+  await supertest(app)
+    .post("/api/auth/verify_signer")
+    .send(data)
+    .expect(401)
+}, 10000);
+
 test("Verify Signer with Data: POST /verify_signer_with_data", async () => {
   await supertest(app)
     .get(
@@ -54,7 +66,7 @@ test("Verify Signer with Data: POST /verify_signer_with_data", async () => {
       const verificationMessage = JSON.parse(response.text);
       const provider = new ethers.getDefaultProvider();
       const signer = new ethers.Wallet(
-        process.env.TEST_WALLET_PRIVATE_KEY,
+        process.env.TEST_WALLET1_PRIVATE_KEY,
         provider
       );
       const signedMessage = await signer.signMessage(verificationMessage);
@@ -75,6 +87,18 @@ test("Verify Signer with Data: POST /verify_signer_with_data", async () => {
     });
 }, 10000);
 
+test("Verify Signer with Data Unauthorized Case: POST /verify_signer_with_data", async () => {
+  const data = {
+    publicKey: "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1",
+    signedMessage: "signedMessage",
+  };
+
+  await supertest(app)
+    .post("/api/auth/verify_signer_with_data")
+    .send(data)
+    .expect(401)
+}, 10000);
+
 test("Api Key: POST /get_api_key", async () => {
   await supertest(app)
     .get(
@@ -85,7 +109,7 @@ test("Api Key: POST /get_api_key", async () => {
       const verificationMessage = JSON.parse(response.text);
       const provider = new ethers.getDefaultProvider();
       const signer = new ethers.Wallet(
-        process.env.TEST_WALLET_PRIVATE_KEY,
+        process.env.TEST_WALLET1_PRIVATE_KEY,
         provider
       );
       const signedMessage = await signer.signMessage(verificationMessage);
@@ -100,8 +124,52 @@ test("Api Key: POST /get_api_key", async () => {
         .expect(200)
         .then((response) => {
           const apiKey = JSON.parse(response.text);
-          console.log(apiKey);
           expect(typeof apiKey).toBe("string");
         });
     });
 }, 10000);
+
+test("Api Key Record Not Found: POST /get_api_key", async () => {
+  const data = {
+    publicKey: "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb1A0",
+    signedMessage: "signedMessage",
+  };
+
+  await supertest(app)
+    .post("/api/auth/get_api_key")
+    .send(data)
+    .expect(404)
+}, 10000);
+
+test("Api Key Record Not Authorized: POST /get_api_key", async () => {
+  const data = {
+    publicKey: "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1",
+    signedMessage: "signedMessage",
+  };
+
+  await supertest(app)
+    .post("/api/auth/get_api_key")
+    .send(data)
+    .expect(401)
+}, 10000);
+
+test("Verify API Key: GET /verify_api_key", async () => {
+  await supertest(app)
+    .get("/api/auth/verify_api_key")
+    .set('Authorization', 'Bearer ' + process.env.LIGHTHOUSE_TESTING_API_KEY)
+    .expect(200)
+}, 10000);
+
+test("Verify API Key Record Not Found: GET /verify_api_key", async () => {
+  await supertest(app)
+    .get("/api/auth/verify_api_key")
+    .set('Authorization', 'Bearer ' + "937b68b8-3768-45d1-950b-30c3836785d5")
+    .expect(404)
+}, 10000);
+
+test("Verify API Key Bad Request: GET /verify_api_key", async () => {
+  await supertest(app)
+    .get("/api/auth/verify_api_key")
+    .expect(400)
+}, 10000);
+
--- controller/authentication/test/verifySignature.test.js
+++ controller/authentication/test/verifySignature.test.js
@@ -24,7 +24,7 @@ test("Verify Signature Not Verified Case: signature verification function", () =
   const verify = verifySignature(
     "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A2",
     "cb3b3bc6-51f8-4e2a-a0de-56fb563f5232",
-    " 0x431e27c431b6fee465356c4e895b4ea67b8bd400132ac2bdf94c29058779e8930a160b501907796866ba34c79f6824a8a3376dcd5e742005c710eff923acbb1d1b"
+    "0x431e27c431b6fee465356c4e895b4ea67b8bd400132ac2bdf94c29058779e8930a160b501907796866ba34c79f6824a8a3376dcd5e742005c710eff923acbb1d1b"
   );
 
   expect(verify).toBe(false);
