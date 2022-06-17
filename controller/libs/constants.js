9f5844249487f58b012c873710ba48a7c4db7743
ravish <ravish1729@gmail.com>
2022-06-17 13:44:49 +0530

    file encryption table added

--- controller/libs/constants.js
+++ controller/libs/constants.js
@@ -1,6 +1,8 @@
 const userTable = "Users";
 const fileTable = "FileManagement";
 const gatewayTable = "SubDomainManagement";
+const fileTableEncryption = "fileTableTest";
+const faucetTable = "faucetTable";
 const freeDataLimitInBytes = 1073741824;
 
-module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable };
+module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable, fileTableEncryption, faucetTable };
