379f79f27b57b803a48ebabe508d236a32676e27
ravish <ravish1729@gmail.com>
2022-06-19 00:32:09 +0530

    updated function

--- controller/encryption/saveFileMetaData.js
+++ controller/encryption/saveFileMetaData.js
@@ -1,14 +1,8 @@
 const dbbClient = require("../libs/ddbClient");
 const { fileTableEncryption } = require("../libs/constants");
-const { v4: uuidv4 } = require("uuid");
 
 module.exports = async (record) => {
   try {
-    const id = uuidv4();
-    const timestamp = Date.now();
-    record["id"] = id;
-    record["createdAt"] = timestamp;
-    record["lastUpdate"] = timestamp;
     const params = {
       TableName: fileTableEncryption,
       Item: record,
