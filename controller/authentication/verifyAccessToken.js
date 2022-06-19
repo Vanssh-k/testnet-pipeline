d62bd2e8edc22363ee7ca69a67b977d138cb3f86
ravish <ravish1729@gmail.com>
2022-06-19 18:16:52 +0530

    implementation improved

--- controller/authentication/verifyAccessToken.js
+++ controller/authentication/verifyAccessToken.js
@@ -1,15 +1,29 @@
 const SHA256 = require("crypto-js/sha256");
 
-module.exports = (record, token) => {
+const dbbClient = require("../libs/ddbClient");
+const { userTable } = require("../libs/constants");
+
+module.exports = async (accessToken) => {
   try {
-    if (
-      record["accessToken"] === SHA256(token).toString() ||
-      record["apiKey"] === SHA256(token).toString()
-    ) {
-      return true;
-    } else {
-      return false;
+    const params = {
+      TableName: userTable,
+      FilterExpression: "accessToken = :a",
+      ExpressionAttributeValues: {
+        ":a": SHA256(accessToken).toString(),
+      },
+    };
+
+    const record = await dbbClient.scan(params).promise();
+
+    if(!record){
+      return null;
     }
+    
+    return {
+      publicKey: record["publicKey"],
+      dataLimit: record["dataLimit"],
+      dataUsed: record["dataUsed"]
+    };
   } catch (error) {
     return null;
   }
