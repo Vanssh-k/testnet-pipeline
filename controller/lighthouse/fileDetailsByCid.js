eaaf6279bbf50cf8f175216f016df56f46da5ff2
ravish <ravish1729@gmail.com>
2022-07-30 00:23:42 +0530

    indexing

--- controller/lighthouse/fileDetailsByCid.js
+++ controller/lighthouse/fileDetailsByCid.js
@@ -2,15 +2,20 @@ const dbbClient = require("../libs/ddbClient");
 const { fileTable } = require("../libs/constants");
 
 module.exports = async (cid) => {
-  const params = {
-    TableName: fileTable,
-    FilterExpression: "cid = :c",
-    ExpressionAttributeValues: {
-      ":c": cid,
-    },
-  };
-
-  const record = await dbbClient.scan(params).promise();
-  const { Items } = record;
-  return Items[0];
+  try{
+    const params = {
+      TableName: fileTable,
+      IndexName: "cid-index",
+      KeyConditionExpression: "cid = :c",
+      ExpressionAttributeValues: {
+        ":c": cid,
+      },
+    };
+  
+    const record = await dbbClient.query(params).promise();
+    const { Items } = record;
+    return Items[0];
+  } catch(error) {
+    return null;
+  }
 };
