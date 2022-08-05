3b244dc610e8a4c2d9f4c379943dbf34fa0ec381
ravish <ravish1729@gmail.com>
2022-08-06 01:52:43 +0530

    bulk cid table add

--- controller/libs/constants.js
+++ controller/libs/constants.js
@@ -2,6 +2,7 @@ const userTable = "UsersTable";
 const fileTable = "files";
 const gatewayTable = "SubDomainManagement";
 const fileTableEncryption = "fileTableTest";
+const bulkCIDAdd = "bulkCIDAdd";
 const freeDataLimitInBytes = 1073741824;
 
-module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable, fileTableEncryption };
+module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable, fileTableEncryption, bulkCIDAdd };
