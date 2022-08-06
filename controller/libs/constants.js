cd6e8a71a67d38c8276f22f8b6d2a07215cd0971
ravish <ravish1729@gmail.com>
2022-08-06 12:28:25 +0530

    added order table

--- controller/libs/constants.js
+++ controller/libs/constants.js
@@ -3,6 +3,7 @@ const fileTable = "files";
 const gatewayTable = "SubDomainManagement";
 const fileTableEncryption = "fileTableTest";
 const bulkCIDAdd = "bulkCIDAdd";
+const cidOrderTable = "cidOrderTable";
 const freeDataLimitInBytes = 1073741824;
 
-module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable, fileTableEncryption, bulkCIDAdd };
+module.exports = { userTable, fileTable, freeDataLimitInBytes, gatewayTable, fileTableEncryption, bulkCIDAdd, cidOrderTable };
