91e8358419b674e3da6cb43a1ac38e3b8847556b
ravish <ravish1729@gmail.com>
2022-08-11 12:48:31 +0530

    bug fix

--- controller/user/index.js
+++ controller/user/index.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
 const userDetails = require("../authentication/userDetails");
 const fileDetails = require("./fileDetails");
 
+const AuthenticationError = require("../../errors/authentication-error");
 const NotFoundError = require("../../errors/not-found-error");
 
 exports.get_uploads = async (req, res, next) => {
@@ -31,6 +33,7 @@ exports.user_data_usage = async (req, res, next) => {
 const verifyJWT = (accessToken, secret) => {
   try {
     const userData = jwt.verify(accessToken, secret);
+    console.log(userData)
     return userData;
   } catch {
     return null;
@@ -40,14 +43,12 @@ const verifyJWT = (accessToken, secret) => {
 exports.faucet_status = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
-
     const userData = verifyJWT(token, process.env.JWT_SECRET);
     if (!userData) {
       throw new AuthenticationError();
     }
 
     const record = await userDetails(userData.publicKey);
-
     res.status(200).json(record["faucet"]);
   } catch (error) {
     next(error);
