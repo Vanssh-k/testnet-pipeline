297ad8f6d342404a4675a8fadc178e7341e8814e
ravish <ravish1729@gmail.com>
2022-09-01 05:01:12 +0530

    updated endpoints

--- controller/gateway/index.js
+++ controller/gateway/index.js
@@ -1,28 +1,43 @@
-const { checkSubdomain, getRecord } = require("../../repository/subdomain");
+const { checkSubdomain, getRecord, userTransactions } = require("../../repository/subdomain");
 const updateSubDomain = require("../../repository/updateSubDomain");
 const NotFoundError = require("../../errors/not-found-error");
 const ForbiddenError = require("../../errors/forbidden");
 
 exports.add_subdomain = async (req, res, next) => {
   try {
+    // TODO apply check for special characters
     const restrictedNames = ["api", "gateway", "testnet", "mainnet", "node"];
     if (restrictedNames.includes(req.body.subDomain)) {
       throw new ForbiddenError();
     }
 
-    const publicKey = req.body.publicKey.toLowerCase();
-
-    const transactionDetails = await getRecord(publicKey);
-    if (transactionDetails.txHash) {
-      transactionDetails.lastUpdate = Date.now();
-      transactionDetails.subDomain = req.body.subDomain;
-
-      const _ = await updateSubDomain(transactionDetails);
-
-      res.status(200).json("SubDomain Created");
-    } else {
+    const exists = await checkSubdomain(req.query.subDomain);
+    if (exists) {
       throw new ForbiddenError();
     }
+
+    const publicKey = req.body.publicKey.toLowerCase();
+
+    const transactionDetails = await userTransactions(publicKey);
+
+    // Temporary Solution, DB redesign required
+    if(transactionDetails.length === 0) {
+      throw new ForbiddenError("No plan purchased");
+    }
+
+    const subscriptionID = transactionDetails[0]["subscriptionID"];
+    const timestamp = Date.now();
+
+    const _ = await updateSubDomain({
+      publicKey: publicKey,
+      subDomain: req.body.subDomain,
+      subscriptionID: subscriptionID,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    });
+
+    res.status(200).json("SubDomain Created");
+    
   } catch (error) {
     next(error);
   }
@@ -30,6 +45,11 @@ exports.add_subdomain = async (req, res, next) => {
 
 exports.check_subdomain = async (req, res, next) => {
   try {
+    const restrictedNames = ["api", "gateway", "testnet", "mainnet", "node"];
+    if (restrictedNames.includes(req.body.subDomain)) {
+      throw new ForbiddenError();
+    }
+    
     const exists = await checkSubdomain(req.query.subDomain);
     if (!exists) {
       throw new NotFoundError();
@@ -40,30 +60,27 @@ exports.check_subdomain = async (req, res, next) => {
   }
 };
 
-exports.get_subdomain = async (req, res, next) => {
-  try {
-    const record = await getRecord(req.query.publicKey);
-    if (!record || !record.subDomain) {
-      throw new NotFoundError();
-    }
-    res.status(200).json(record.subDomain);
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.get_transaction_details = async (req, res, next) => {
+exports.user_domain = async (req, res, next) =>{
   try {
     const record = await getRecord(req.query.publicKey);
     if (!record) {
       throw new NotFoundError();
     }
-    res.status(200).json({
-      network: record.network,
-      subDomain: record.subDomain,
-      txHash: record.txHash,
-      value: record.value,
-    });
+
+    res.status(200).json(record);
+  } catch (error) {
+    next(error);
+  }
+}
+
+exports.get_transaction_details = async (req, res, next) => {
+  try {
+    const record = await userTransactions(req.query.publicKey);
+    if (!record) {
+      throw new NotFoundError();
+    }
+
+    res.status(200).json(record);
   } catch (error) {
     next(error);
   }
