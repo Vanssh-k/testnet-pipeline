50c79a7ef43fdcdc7ef08e21951102313695e30f
ravish <ravish1729@gmail.com>
2022-09-01 05:01:55 +0530

    corrections

--- middlewares/validators/gateway.js
+++ middlewares/validators/gateway.js
@@ -4,8 +4,8 @@ module.exports.addSubdomainSchema = joi.object({
   subDomain: joi.string().required().messages({
     "any.required": `subDomain not found`,
   }),
-  signedMesage: joi.string().required().messages({
-    "any.required": `signedMesage not found`,
+  signedMessage: joi.string().required().messages({
+    "any.required": `signedMessage not found`,
   }),
   publicKey: joi.string().required().messages({
     "any.required": `publicKey not found`,
