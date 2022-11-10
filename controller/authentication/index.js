136c8cf268b525870abe8e34385ee694b27e1dcf
ravish <ravish1729@gmail.com>
2022-11-11 04:36:28 +0530

    implemented helpers

--- controller/authentication/index.js
+++ controller/authentication/index.js
@@ -1,43 +1,22 @@
-const SHA256 = require("crypto-js/sha256");
-const { v4: uuidv4 } = require("uuid");
-const jwt = require("jsonwebtoken");
-const checkTwitter = require("./checkTwitter");
-const updateUserDetails = require("../../repository/updateUserDetails");
-const { freeDataLimitInBytes } = require("../libs/constants");
-const ForbiddenError = require("../../errors/forbidden");
+const { tweetRecharge } = require("./helper/tweetHelper");
+const { getMessage, verifySigner, refreshAccessToken, removeRefreshToken, getApiKey } = require("./helper/authHelper");
+
+// Get message - user will sign this message to verify himself
+exports.get_message = async (req, res, next) => {
+  try {
+    const message = await getMessage(req.query.publicKey, req.network, req.user);
+    res.status(200).json(message);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Return access token if user is authentic
 exports.verify_signer = async (req, res, next) => {
   try {
     const record = req.user;
-    // Change the message and return access token
-    const payLoad = { publicKey: record.publicKey };
-    const accessToken = jwt.sign(payLoad, process.env.JWT_SECRET, {
-      algorithm: "HS256",
-      expiresIn: "12h",
-    });
-    const refreshToken = jwt.sign(payLoad, process.env.JWT_REFRESH_SECRET, {
-      algorithm: "HS256",
-    });
-
-    const updatedDetails = {
-      publicKey: record.publicKey,
-      message: Date.now(),
-      dataLimit: record.dataLimit,
-      dataUsed: record.dataUsed,
-      apiKey: record.apiKey,
-      refreshToken: SHA256(refreshToken).toString(),
-      faucet: record.faucet,
-      network: record.network,
-      createdAt: record.createdAt,
-      updatedAt: Date.now()
-    };
-
-    const _ = await updateUserDetails(updatedDetails);
-
-    res
-      .status(200)
-      .json({ accessToken: accessToken, refreshToken: refreshToken });
+    const token = await verifySigner(record);
+    res.status(200).json(token);
   } catch (error) {
     next(error);
   }
@@ -60,75 +39,17 @@ exports.verify_access_token = async (req, res, next) => {
 
 exports.refresh_access_token = async (req, res, next) => {
   try {
-    const record = req.user;
-    const payLoad = { publicKey: record.publicKey };
-    const accessToken = jwt.sign(payLoad, process.env.JWT_SECRET, {
-      algorithm: "HS256",
-      expiresIn: "12h",
-    });
-
-    res.status(200).json({ accessToken: accessToken });
+    const newAccessToken = refreshAccessToken(req.user);
+    res.status(200).json(newAccessToken);
   } catch (error) {
     next(error);
   }
 };
 
-exports.remove_refresh_access_token = async (req, res, next) => {
+exports.remove_refresh_token = async (req, res, next) => {
   try {
-    const record = req.user;
-
-    const updatedDetails = {
-      publicKey: record.publicKey,
-      message: record.message,
-      dataLimit: record.dataLimit,
-      dataUsed: record.dataUsed,
-      apiKey: record.apiKey,
-      refreshToken: SHA256(Date.now()).toString(),
-      faucet: record.faucet,
-      network: record.network,
-      createdAt: record.createdAt,
-      updatedAt: Date.now()
-    };
-
-    const _ = await updateUserDetails(updatedDetails);
-
-    res.status(200).json("Logout Success!!!");
-  } catch (error) {
-    next(error);
-  }
-};
-
-// Get message - user will sign this message to verify himself
-exports.get_message = async (req, res, next) => {
-  try {
-    const publicKey = req.query.publicKey;
-    const network = req.network;
-    const record = req.user;
-
-    const timestamp = Date.now();
-    const message = 
-      "Please prove you are the owner of this wallet by signing this message, nonce=" + timestamp;
-
-    const updatedDetails = {
-      publicKey: publicKey,
-      message: message,
-      dataLimit: record ? record.dataLimit : freeDataLimitInBytes,
-      dataUsed: record ? record.dataUsed : 0,
-      apiKey: record
-        ? record.apiKey === ""
-          ? SHA256(uuidv4().toString()).toString()
-          : record.apiKey
-        : SHA256(uuidv4().toString()).toString(),
-      refreshToken: record ? record.refreshToken : SHA256(Date.now()).toString(),
-      faucet: record ? record.faucet : {},
-      network: network,
-      createdAt: record ? record.createdAt : timestamp,
-      updatedAt: timestamp
-    };
-
-    const _ = await updateUserDetails(updatedDetails, network);
-
-    res.status(200).json(message);
+    const response = removeRefreshToken(req.user);
+    res.status(200).json(response);
   } catch (error) {
     next(error);
   }
@@ -136,23 +57,7 @@ exports.get_message = async (req, res, next) => {
 
 exports.get_api_key = async (req, res, next) => {
   try {
-    const record = req.user;
-    const apiKey = uuidv4().toString();
-    const updatedDetails = {
-      publicKey: record.publicKey,
-      message: uuidv4().toString(),
-      dataLimit: record.dataLimit,
-      dataUsed: record.dataUsed,
-      apiKey: SHA256(apiKey).toString(),
-      refreshToken: record.refreshToken,
-      faucet: record.faucet,
-      network: record.network,
-      createdAt: record.createdAt,
-      updatedAt: Date.now()
-    };
-
-    const _ = await updateUserDetails(updatedDetails);
-
+    const apiKey = await getApiKey(req.user);
     res.status(200).json(apiKey);
   } catch (error) {
     next(error);
@@ -174,36 +79,7 @@ exports.verify_api_key = async (req, res, next) => {
 
 exports.tweet_recharge = async (req, res, next) => {
   try {
-    const twitterID = req.query.twitterID;
-    const record = req.user;
-
-    // Check if user have already used faucet
-    if (record["faucet"]["twitter"] === "used") {
-      throw new ForbiddenError();
-    }
-
-    // Check for validity of tweet
-    const validTweet = await checkTwitter(record.publicKey, twitterID);
-    if (!validTweet) {
-      throw new ForbiddenError();
-    }
-
-    // Update Data Limit
-    const updatedDetails = {
-      publicKey: record.publicKey,
-      message: record.message,
-      dataLimit: record.dataLimit + freeDataLimitInBytes,
-      dataUsed: record.dataUsed,
-      apiKey: record.apiKey,
-      refreshToken: record.refreshToken,
-      faucet: { twitter: "used" },
-      network: record.network,
-      createdAt: record.createdAt,
-      updatedAt: Date.now()
-    };
-
-    const _ = await updateUserDetails(updatedDetails);
-
+    const _ = await tweetRecharge(req.user, req.query.twitterID);
     res.status(200).json("Data Limit Upgraded");
   } catch (error) {
     next(error);
