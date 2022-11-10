388d32ab3aec9e158bfed4548358d6a592daaa00
ravish <ravish1729@gmail.com>
2022-11-11 04:46:39 +0530

    endpoint name updated

--- controller/governance/index.test.js
+++ controller/governance/index.test.js
@@ -1,23 +1,24 @@
 const app = require("../../app");
 const supertest = require("supertest");
 
-// add_proposal
-test("Add Proposal: POST /add_proposal", async () => {
+// create_proposal
+test("Add Proposal: POST /create_proposal", async () => {
   const data = {
     "publicKey": "0x487fc2fE07c593EAb555729c3DD6dF85020B5160",
     "proposal": "Test"
   };
 
   await supertest(app)
-    .post("/api/governance/add_proposal")
+    .post("/api/governance/create_proposal")
     .send(data)
     .expect(200)
 }, 30000);
 
-test("Get Proposals: GET /get_proposals", async () => {
+// list_proposals
+test("Get Proposals: GET /list_proposals", async () => {
 
   await supertest(app)
-    .get("/api/governance/get_proposals")
+    .get("/api/governance/list_proposals")
     .expect(200)
     .then((response) => {
       const proposals = JSON.parse(response.text);
--- routes/governance.js
+++ routes/governance.js
@@ -6,14 +6,14 @@ const validator = require("../middlewares/validators");
 const router = express.Router();
 
 router.post(
-  "/add_proposal",
+  "/create_proposal",
   validate(validator.addProposalSchema, { body: true }),
-  GovernanceController.add_proposal
+  GovernanceController.create_proposal
 );
 
 router.get(
-  "/get_proposals",
-  GovernanceController.all_proposals
+  "/list_proposals",
+  GovernanceController.list_proposals
 );
 
 module.exports = router;
