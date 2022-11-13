b15a437b58ad80bf037130b3be4cda91d66077b7
ravish <ravish1729@gmail.com>
2022-11-14 03:42:01 +0530

    lint

--- routes/governance.js
+++ routes/governance.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const GovernanceController = require("../controller/governance");
-const validate = require("../middlewares/validate");
-const validator = require("../middlewares/validators");
+const express = require('express');
+const GovernanceController = require('../controller/governance');
+const validate = require('../middlewares/validate');
+const validator = require('../middlewares/validators');
 
 const router = express.Router();
 
 router.post(
-  "/create_proposal",
+  '/create_proposal',
   validate(validator.addProposalSchema, { body: true }),
-  GovernanceController.create_proposal
+  GovernanceController.create_proposal,
 );
 
 router.get(
-  "/list_proposals",
-  GovernanceController.list_proposals
+  '/list_proposals',
+  GovernanceController.list_proposals,
 );
 
 module.exports = router;
--- routes/lighthouse.js
+++ routes/lighthouse.js
@@ -1,66 +1,66 @@
-const express = require("express");
-const LighthouseController = require("../controller/lighthouse");
-const authenticator = require("../middlewares/authenticator");
-const validate = require("../middlewares/validate");
-const validator = require("../middlewares/validators");
+const express = require('express');
+const LighthouseController = require('../controller/lighthouse');
+const authenticator = require('../middlewares/authenticator');
+const validate = require('../middlewares/validate');
+const validator = require('../middlewares/validators');
 
 const router = express.Router();
 
 router.get(
-  "/get_ticker",
+  '/get_ticker',
   validate(validator.symbolSchema, { query: true }),
-  LighthouseController.get_ticker
+  LighthouseController.get_ticker,
 );
 
 router.get(
-  "/file_info",
+  '/file_info',
   validate(validator.cidSchema, { query: true }),
-  LighthouseController.file_info
+  LighthouseController.file_info,
 );
 
 router.get(
-  "/cid_status",
+  '/cid_status',
   validate(validator.cidSchema, { query: true }),
-  LighthouseController.cid_status
+  LighthouseController.cid_status,
 );
 
 router.post(
-  "/add_cid",
+  '/add_cid',
   validate(validator.addCidSchema, { body: true }),
-  LighthouseController.add_cid
+  LighthouseController.add_cid,
 );
 
 router.post(
-  "/migration_request",
+  '/migration_request',
   validate(validator.migrationRequestSchema, { body: true }),
-  authenticator(["verifysignature"]),
-  LighthouseController.migration_request
+  authenticator(['verifysignature']),
+  LighthouseController.migration_request,
 );
 
 router.post(
-  "/migration_request_ent",
+  '/migration_request_ent',
   validate(validator.migrationRequestEntSchema, { body: true }),
-  authenticator(["enterpriseRoute"]),
-  LighthouseController.migration_request_ent
+  authenticator(['enterpriseRoute']),
+  LighthouseController.migration_request_ent,
 );
 
 router.get(
-  "/list_migration_requests",
+  '/list_migration_requests',
   validate(validator.publicKeySchema, { query: true }),
-  LighthouseController.list_migration_requests
+  LighthouseController.list_migration_requests,
 );
 
 router.get(
-  "/migration_request_info",
+  '/migration_request_info',
   validate(validator.migrationRequestIdSchema, { query: true }),
-  LighthouseController.migration_request_info
+  LighthouseController.migration_request_info,
 );
 
 router.post(
-  "/add_cid_to_queue",
+  '/add_cid_to_queue',
   validate(validator.addCIDToQueueSchema, { body: true }),
-  authenticator(["verifypublickey"], ["protectedRoute"]),
-  LighthouseController.add_cid_to_queue
+  authenticator(['verifypublickey'], ['protectedRoute']),
+  LighthouseController.add_cid_to_queue,
 );
 
 module.exports = router;
