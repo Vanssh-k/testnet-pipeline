a2759eecc7a9734c7d3287b8a17ee23bd3f186e1
ravish <ravish1729@gmail.com>
2022-11-14 03:40:32 +0530

    moved to users

--- repository/user/updateUserDetails.js
+++ repository/user/updateUserDetails.js
@@ -1,16 +1,17 @@
-const { v4: uuidv4 } = require("uuid");
-const dbbClient = require("./ddbClient");
-const SHA256 = require("crypto-js/sha256");
-const { userTable } = require("../controller/libs/constants");
+const { v4: uuidv4 } = require('uuid');
+const SHA256 = require('crypto-js/sha256');
+const dbbClient = require('../ddbClient');
+const { userTable } = require('../../controller/libs/constants');
+const DatabaseError = require('../../errors/database-error');
 
 module.exports = async (updatedDetails, network) => {
   try {
-    if(network==="evm"){
+    if (network === 'evm') {
       updatedDetails.publicKey = updatedDetails.publicKey.trim().toLowerCase();
     }
-    
+
     // Secondary Index null case
-    if(updatedDetails.apiKey === ""){
+    if (updatedDetails.apiKey === '') {
       updatedDetails.apiKey = SHA256(uuidv4().toString()).toString();
     }
 
@@ -20,9 +21,8 @@ module.exports = async (updatedDetails, network) => {
     };
 
     await dbbClient.put(params).promise();
-    return "Put Successful";
+    return 'Put Successful';
   } catch (error) {
-    console.error(error);
-    return null;
+    throw new DatabaseError();
   }
 };
