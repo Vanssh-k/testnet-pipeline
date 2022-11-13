29ceb2c69f30e927c012def2ae867a3234de7aba
ravish <ravish1729@gmail.com>
2022-11-14 03:41:49 +0530

    lint

--- utils/verifyCID.js
+++ utils/verifyCID.js
@@ -1,7 +1,7 @@
 module.exports = (cid) => {
-  if (cid.startsWith("Qm")) {
+  if (cid.startsWith('Qm')) {
     return /^[A-HJ-NP-Za-km-z1-9]*$/.test(cid) && cid.length == 46;
-  } else if (cid.startsWith("b")) {
+  } if (cid.startsWith('b')) {
     return cid.length >= 50;
   }
   return false;
