5d8e3fdc648b0d09e8722a990437477fe1ac4f65
ravish <ravish1729@gmail.com>
2022-11-14 03:43:02 +0530

    renamed from fileDetails

---
+++ repository/file/userUploads.js
@@ -0,0 +1,23 @@
+const dbbClient = require('../ddbClient');
+const { fileTable } = require('../../controller/libs/constants');
+const DatabaseError = require('../../errors/database-error');
+
+module.exports = async (usersPublicKey) => {
+  try {
+    const params = {
+      TableName: fileTable,
+      IndexName: 'publicKey-createdAt-index',
+      ScanIndexForward: false,
+      KeyConditionExpression: 'publicKey = :p',
+      ExpressionAttributeValues: {
+        ':p': usersPublicKey,
+      },
+    };
+
+    const record = await dbbClient.query(params).promise();
+    const { Items } = record;
+    return Items;
+  } catch (error) {
+    throw new DatabaseError();
+  }
+};
