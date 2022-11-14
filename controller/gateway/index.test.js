39a5449322fb515f0e1ae3497ade5c60b13c49fc
ravish <ravish1729@gmail.com>
2022-11-14 12:25:15 +0530

    lint

--- controller/gateway/index.test.js
+++ controller/gateway/index.test.js
@@ -1,12 +1,12 @@
-const app = require("../../app");
-const supertest = require("supertest");
-const ethers = require("ethers");
+const supertest = require('supertest');
+const ethers = require('ethers');
+const app = require('../../app');
 
 // add_subdomain
-test("Add SubDomain Main Case: POST /add_subdomain", async () => {
+test('Add SubDomain Main Case: POST /add_subdomain', async () => {
   await supertest(app)
     .get(
-      "/api/auth/get_message?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475"
+      '/api/auth/get_message?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475',
     )
     .expect(200)
     .then(async (response) => {
@@ -14,30 +14,30 @@ test("Add SubDomain Main Case: POST /add_subdomain", async () => {
       const provider = new ethers.getDefaultProvider();
       const signer = new ethers.Wallet(
         process.env.TEST_WALLET6_PRIVATE_KEY,
-        provider
+        provider,
       );
       const signedMessage = await signer.signMessage(verificationMessage);
       const data = {
-        publicKey: "0x75a22ede971080c8448c46de6ae5df3f64c67475",
-        signedMessage: signedMessage,
-        subDomain: "rav"
+        publicKey: '0x75a22ede971080c8448c46de6ae5df3f64c67475',
+        signedMessage,
+        subDomain: 'rav',
       };
 
       await supertest(app)
-        .post("/api/gateway/add_subdomain")
+        .post('/api/gateway/add_subdomain')
         .send(data)
         .expect(200)
         .then((response) => {
           const message = JSON.parse(response.text);
-          expect(message).toBe("SubDomain Created");
+          expect(message).toBe('SubDomain Created');
         });
     });
 }, 30000);
 
-test("Add SubDomain Forbidden: POST /add_subdomain", async () => {
+test('Add SubDomain Forbidden: POST /add_subdomain', async () => {
   await supertest(app)
     .get(
-      "/api/auth/get_message?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475"
+      '/api/auth/get_message?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475',
     )
     .expect(200)
     .then(async (response) => {
@@ -45,83 +45,83 @@ test("Add SubDomain Forbidden: POST /add_subdomain", async () => {
       const provider = new ethers.getDefaultProvider();
       const signer = new ethers.Wallet(
         process.env.TEST_WALLET6_PRIVATE_KEY,
-        provider
+        provider,
       );
       const signedMessage = await signer.signMessage(verificationMessage);
       const data = {
-        publicKey: "0x75a22ede971080c8448c46de6ae5df3f64c67475",
-        signedMessage: signedMessage,
-        subDomain: "api"
+        publicKey: '0x75a22ede971080c8448c46de6ae5df3f64c67475',
+        signedMessage,
+        subDomain: 'api',
       };
 
       await supertest(app)
-        .post("/api/gateway/add_subdomain")
+        .post('/api/gateway/add_subdomain')
         .send(data)
         .expect(403);
     });
 }, 30000);
 
-test("Add SubDomain Authentication Error: POST /add_subdomain", async () => {
+test('Add SubDomain Authentication Error: POST /add_subdomain', async () => {
   const data = {
-    publicKey: "0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1",
-    signedMessage: "signedMessage",
-    subDomain: "kkkk"
+    publicKey: '0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1',
+    signedMessage: 'signedMessage',
+    subDomain: 'kkkk',
   };
 
   await supertest(app)
-    .post("/api/gateway/add_subdomain")
+    .post('/api/gateway/add_subdomain')
     .send(data)
     .expect(401);
 }, 30000);
 
 // check_subdomain
-test("Check Sub Domain Main Case: GET /check_subdomain", async () => {
+test('Check Sub Domain Main Case: GET /check_subdomain', async () => {
   await supertest(app)
-    .get("/api/gateway/check_subdomain?subDomain=rav")
+    .get('/api/gateway/check_subdomain?subDomain=rav')
     .expect(200)
     .then((response) => {
       const exists = JSON.parse(response.text);
-      expect(exists).toBe("Exists");
+      expect(exists).toBe('Exists');
     });
 }, 30000);
 
-test("Check Sub Domain Not Found: GET /check_subdomain", async () => {
+test('Check Sub Domain Not Found: GET /check_subdomain', async () => {
   await supertest(app)
-    .get("/api/gateway/check_subdomain?subDomain=gateway")
+    .get('/api/gateway/check_subdomain?subDomain=gateway')
     .expect(404);
 }, 30000);
 
 // get_subdomain
-test("Get Sub Domain Main Case: GET /get_subdomain", async () => {
+test('Get Sub Domain Main Case: GET /get_subdomain', async () => {
   await supertest(app)
-    .get("/api/gateway/get_subdomain?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475")
+    .get('/api/gateway/get_subdomain?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475')
     .expect(200)
     .then((response) => {
       const subDomain = JSON.parse(response.text);
-      expect(subDomain).toBe("rav");
+      expect(subDomain).toBe('rav');
     });
 }, 30000);
 
-test("Get Sub Domain Not Found: GET /get_subdomain", async () => {
+test('Get Sub Domain Not Found: GET /get_subdomain', async () => {
   await supertest(app)
-    .get("/api/gateway/get_subdomain?publicKey=0x487fc2fE07c593EAb555729c3DD6dF85020B5161")
+    .get('/api/gateway/get_subdomain?publicKey=0x487fc2fE07c593EAb555729c3DD6dF85020B5161')
     .expect(404);
 }, 30000);
 
 // get_transaction_details
-test("Get Transaction Details Main Case: GET /get_transaction_details", async () => {
+test('Get Transaction Details Main Case: GET /get_transaction_details', async () => {
   await supertest(app)
-    .get("/api/gateway/get_transaction_details?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475")
+    .get('/api/gateway/get_transaction_details?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67475')
     .expect(200)
     .then((response) => {
       const txDetails = JSON.parse(response.text);
-      expect(typeof txDetails[0].network).toBe("string");
+      expect(typeof txDetails[0].network).toBe('string');
     });
 }, 30000);
 
-test("Get Transaction Details Record Not Found: GET /get_transaction_details", async () => {
+test('Get Transaction Details Record Not Found: GET /get_transaction_details', async () => {
   await supertest(app)
-    .get("/api/gateway/get_transaction_details?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67435")
+    .get('/api/gateway/get_transaction_details?publicKey=0x75a22ede971080c8448c46de6ae5df3f64c67435')
     .expect(200)
     .then((response) => {
       const txDetails = JSON.parse(response.text);
