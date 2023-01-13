059df8e54bfca6a3247655ed10d3c407de1cc242
xlassix <46370698+xlassix@users.noreply.github.com>
2023-01-13 12:32:20 +0100

    redis set up

---
+++ repository/cacheClient.js
@@ -0,0 +1,52 @@
+const Redis = require("ioredis");
+
+const redis = new Redis();
+
+module.exports.setCache = async (key, value) => {
+  return redis.set(key, JSON.stringify(value));
+};
+
+module.exports.getCache = async (key) => {
+  return redis
+    .get(key)
+    .then((result) => {
+      return JSON.parse(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+module.exports.removeCache = async (key) => {
+  return redis
+    .del(key)
+    .then((result) => {
+      return JSON.parse(result);
+    })
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
+};
+
+module.exports.cacheFunction = async (fn, key) => {
+  let data = await this.getCache(key);
+  if (data) {
+    return data;
+  }
+  data = await fn();
+  await this.setCache(key, data);
+  return data;
+};
+
+module.exports.clearCacheStartsWith = async (keyword) => {
+  let cursor = "0";
+  do {
+    const [nextCursor, keys] = await redis.scan(cursor, "MATCH", `${keyword}*`);
+    cursor = nextCursor;
+    if (keys.length > 0) {
+      await redis.del(keys);
+    }
+  } while (cursor !== "0");
+};
