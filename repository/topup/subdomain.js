b01045eb7fa7330120ba1368132b2ce8c4e09fe1
ravish <ravish1729@gmail.com>
2023-02-07 08:05:52 +0530

    moved into topup folder

---
+++ repository/topup/subdomain.js
@@ -0,0 +1,56 @@
+const dbbClient = require('../ddbClient');
+const { gatewayTable } = require('../../controller/libs/constants');
+
+const checkSubdomain = async (subDomain) => {
+  try {
+    const params = {
+      TableName: gatewayTable,
+      FilterExpression: 'subDomain = :s',
+      ExpressionAttributeValues: {
+        ':s': subDomain,
+      },
+    };
+
+    const record = await dbbClient.scan(params).promise();
+    const { Items } = record;
+    return Items[0];
+  } catch (error) {
+    return null;
+  }
+};
+
+const getRecord = async (publicKey) => {
+  try {
+    const params = {
+      TableName: gatewayTable,
+      Key: {
+        publicKey: publicKey.toLowerCase(),
+      },
+    };
+
+    const record = await dbbClient.get(params).promise();
+    return record.Item;
+  } catch (error) {
+    return null;
+  }
+};
+
+const updateSubDomain = async (transactionDetails) => {
+  try {
+    const params = {
+      TableName: gatewayTable,
+      Item: transactionDetails,
+    };
+
+    await dbbClient.put(params).promise();
+    return 'Put Successful';
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  checkSubdomain,
+  getRecord,
+  updateSubDomain,
+};
---
+++ repository/topup/userTransactions.js
@@ -0,0 +1,22 @@
+const dbbClient = require('../ddbClient');
+const {
+  subscriptionPurchaseTransactions
+} = require('../../controller/libs/constants');
+
+exports.userTransactions = async (publicKey) => {
+  try {
+    const params = {
+      TableName: subscriptionPurchaseTransactions,
+      FilterExpression: 'publicKey = :p',
+      ExpressionAttributeValues: {
+        ':p': publicKey.toLowerCase(),
+      },
+    };
+
+    const record = await dbbClient.scan(params).promise();
+    const { Items } = record;
+    return Items;
+  } catch (error) {
+    return null;
+  }
+};
