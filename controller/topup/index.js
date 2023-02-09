285fbda5292b2102b375cee4b466f457fb384476
ravish <ravish1729@gmail.com>
2023-02-10 01:56:29 +0530

    fixed parameter

--- controller/topup/index.js
+++ controller/topup/index.js
@@ -74,7 +74,7 @@ exports.get_active_plan_list = async (req, res, next) => {
 
 exports.users_active_plan = async (req, res, next) => {
     try {
-        const data = await usersActivePlan(req.body.publicKey)
+        const data = await usersActivePlan(req.query.publicKey)
         res.status(data.status).json({ data: data.data })
     } catch (error) {
         next(error)
@@ -90,11 +90,12 @@ exports.plan_details_by_id = async (req, res, next) => {
     }
 }
 
-exports.activate_plan = async (req, res, next) => {
-    try {
-        const data = await activatePlan(req.query.publicKey)
-        res.status(data.status).json({ data: data.data })
-    } catch (error) {
-        next(error)
-    }
-}
+// exports.activate_plan = async (req, res, next) => {
+//     try {
+//         const record = await userDetails(req.query.publicKey, "evm")
+//         const data = await activatePlan(record)
+//         res.status(data.status).json({ data: data.data })
+//     } catch (error) {
+//         next(error)
+//     }
+// }
