5f9c52b3da2caf138cc6298681735a5ac74f579d
ravish <ravish1729@gmail.com>
2023-02-16 00:22:19 +0530

    added plans

--- controller/libs/constants.js
+++ controller/libs/constants.js
@@ -10,6 +10,10 @@ const subscriptionPlans = 'subscriptionPlans'
 const messageString =
     'Please prove you are the owner of this wallet by signing this message, nonce='
 const freeDataLimitInBytes = 1073741824
+const subscriptionPlanDetails = [
+    {index:0,planName:'Spark',totalNumOfDeduction:1,nextDeductionInNumOfBlocks:1200000,amount:10000000,totalFilesPinned:10000,ipfsGBStorage:30,filecoinPermanentStorage:'on demand',dedicatedGateway:1,bandwidthInGB:60,requests:100000,encryptionAccessControl:'mainnet',imageResize:'yes',techSupportCall:'no',payInCrypto:'yes',filecoinDeals:'yes'},
+    {index:1,planName:'Blaze',totalNumOfDeduction:1,nextDeductionInNumOfBlocks:1200000,amount:50000000,totalFilesPinned:50000,ipfsGBStorage:150,filecoinPermanentStorage:'on demand',dedicatedGateway:3,bandwidthInGB:450,requests:1000000,encryptionAccessControl:'mainnet',imageResize:'yes',techSupportCall:'yes',payInCrypto:'yes',filecoinDeals:'yes'}
+]
 
 module.exports = {
     userTable,
@@ -23,4 +27,5 @@ module.exports = {
     ProposalTable,
     subscriptionPlans,
     messageString,
+    subscriptionPlanDetails
 }
