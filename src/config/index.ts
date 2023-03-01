40558cbaf2ac9d9f13aed3ccc792fe7f830043fc
xlassix <awhy14539@gmail.com>
2023-03-01 17:53:36 +0100

    env config

---
+++ src/config/index.ts
@@ -0,0 +1,85 @@
+import dotenv from 'dotenv'
+import Joi from 'joi'
+
+dotenv.config()
+const env = process.env.NODE_ENV || 'development'
+
+const baseConfig = {
+    env,
+    isDev: env === 'development',
+    isTest: env === 'testing',
+    est_api_key: process.env.EST_API_KEY ?? '',
+    covalent_api_key: process.env.COVALENT_API_KEY ?? '',
+    aws_access_key_id: process.env.AWS_ACCESS_KEY_ID ?? '',
+    aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    cloudflare_key: process.env.CLOUDFLARE_KEY ?? '',
+    transaction_route_token: process.env.TRANSACTION_ROUTE_TOKEN ?? '',
+    polygon_rpc: process.env.POLYGON_RPC ?? '',
+    migration_test_access_token: process.env.MIGRATION_TEST_ACCESS_TOKEN ?? '',
+    route_access_token: process.env.ROUTE_ACCESS_TOKEN ?? '',
+    message_auth_token: process.env.MESSAGE_AUTH_TOKEN ?? '',
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET ?? '',
+    jwt_secret: process.env.JWT_SECRET ?? '',
+    twitter_api_key: process.env.TWITTER_API_KEY ?? '',
+    test_wallet1_private_key: process.env.TEST_WALLET1_PRIVATE_KEY ?? '',
+    test_wallet2_private_key: process.env.TEST_WALLET2_PRIVATE_KEY ?? '',
+    test_wallet3_private_key: process.env.TEST_WALLET3_PRIVATE_KEY ?? '',
+    test_wallet4_private_key: process.env.TEST_WALLET4_PRIVATE_KEY ?? '',
+    test_wallet5_private_key: process.env.TEST_WALLET5_PRIVATE_KEY ?? '',
+    test_wallet6_private_key: process.env.TEST_WALLET6_PRIVATE_KEY ?? '',
+    migration_ocean_access_token:
+        process.env.MIGRATION_OCEAN_ACCESS_TOKEN ?? '',
+    port: process.env.PORT ?? 8000,
+}
+
+const envVarsSchema = Joi.object({
+    port: Joi.number().default(3000),
+    aws_access_key_id: Joi.string().required().messages({
+        'any.required': `'AWS_ACCESS_KEY_ID IS MISSING'`,
+    }),
+    aws_secret_access_key: Joi.string().required().messages({
+        'any.required': `'AWS_SECRET_ACCESS_KEY IS MISSING'`,
+    }),
+    cloudflare_key: Joi.string().required().messages({
+        'any.required': `'CLOUDFLARE_KEY IS MISSING'`,
+    }),
+    transaction_route_token: Joi.string().required().messages({
+        'any.required': `'TRANSACTION_ROUTE_TOKEN IS MISSING'`,
+    }),
+    polygon_rpc: Joi.string().required().messages({
+        'any.required': `'POLYGON_RPC IS MISSING'`,
+    }),
+    migration_test_access_token: Joi.string().required().messages({
+        'any.required': `'MIGRATION_TEST_ACCESS_TOKEN IS MISSING'`,
+    }),
+    route_access_token: Joi.string().required().messages({
+        'any.required': `'ROUTE_ACCESS_TOKEN IS MISSING'`,
+    }),
+    message_auth_token: Joi.string().required().messages({
+        'any.required': `'MESSAGE_AUTH_TOKEN IS MISSING'`,
+    }),
+    jwt_refresh_secret: Joi.string().required().messages({
+        'any.required': `'JWT_REFRESH_SECRET IS MISSING'`,
+    }),
+    jwt_secret: Joi.string().required().messages({
+        'any.required': `'JWT_SECRET IS MISSING'`,
+    }),
+    twitter_api_key: Joi.string().required().messages({
+        'any.required': `'TWITTER_API_KEY IS MISSING'`,
+    }),
+    migration_ocean_access_token: Joi.string(),
+    test_wallet1_private_key: Joi.string(),
+    test_wallet2_private_key: Joi.string(),
+    test_wallet3_private_key: Joi.string(),
+    test_wallet4_private_key: Joi.string(),
+    test_wallet5_private_key: Joi.string(),
+    test_wallet6_private_key: Joi.string(),
+}).unknown()
+
+const { value: envVars, error } = envVarsSchema.validate(baseConfig)
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`)
+}
+
+export default baseConfig
