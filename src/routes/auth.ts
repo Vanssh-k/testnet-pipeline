0b75eae0f2ebc261725c484b81cbf9866ee713f2
ravish <ravish1729@gmail.com>
2023-03-26 15:29:36 +0530

    updated with noew auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -6,8 +6,10 @@ import {
     refresh_access_token,
     remove_refresh_token,
     get_message,
-    get_api_key,
     tweet_recharge,
+    remove_api_key,
+    create_api_key,
+    get_user_keys
 } from '../controller/authentication'
 import validator from '../middlewares/validators'
 import validate from '../middlewares/validate'
@@ -24,19 +26,19 @@ router.post(
 
 router.get(
     '/verify_access_token',
-    authenticator(['verifyjwt']),
+    authenticator(['verifyToken']),
     verify_access_token
 )
 
 router.get(
     '/refresh_access_token',
-    authenticator(['verifyjwt'], ['useRefreshSecret', 'useRefreshEquality']),
+    authenticator(['verifyToken'], ['useRefreshSecret', 'useRefreshEquality']),
     refresh_access_token
 )
 
 router.delete(
     '/remove_refresh_token',
-    authenticator(['verifyjwt'], ['useRefreshSecret', 'useRefreshEquality']),
+    authenticator(['verifyToken'], ['useRefreshSecret', 'useRefreshEquality']),
     remove_refresh_token
 )
 
@@ -51,20 +53,40 @@ router.post(
     '/get_api_key',
     validate(validator.verifySignerSchema, { body: true }),
     authenticator(['verifysignature']),
-    get_api_key
+    create_api_key
+)
+
+router.post(
+    '/create_api_key',
+    validate(validator.verifySignerSchema, { body: true }),
+    authenticator(['verifysignature']),
+    create_api_key
+)
+
+router.get(
+    '/verify_api_key',
+    authenticator(['verifyToken']),
+    verify_api_key
+)
+
+router.get(
+    '/get_user_keys',
+    authenticator(['verifyToken'], ['publicKeyOnly']),
+    get_user_keys
+)
+
+router.delete(
+    '/remove_api_key',
+    validate(validator.apiKeyIdSchema, { query: true }),
+    authenticator(['verifyToken'], ['publicKeyOnly']),
+    remove_api_key
 )
 
 router.get(
     '/tweet_recharge',
     validate(validator.tweetRechargeSchema, { query: true }),
-    authenticator(['verifyjwt']),
+    authenticator(['verifyToken']),
     tweet_recharge
-)
-
-router.get(
-    '/verify_api_key',
-    authenticator(['verifyjwt'], ['useSHA256WithApiKey']),
-    verify_api_key
 )
 
 export default router
