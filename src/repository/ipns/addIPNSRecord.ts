35fb6489a1eaba23e5f0f541daf72c683400308f
ravish <ravish1729@gmail.com>
2023-03-28 22:13:30 +0530

    add ipns records

--- src/repository/ipns/addIPNSRecord.ts
+++ src/repository/ipns/addIPNSRecord.ts
@@ -1,17 +1,18 @@
 import dbbClient from '../ddbClient'
+import { addIPNSRecord } from './types'
 import { ipnsTable } from '../../controller/libs/constants'
 import DatabaseError from '../../errors/database-error'
 
-export default async (record: any) => {
-    try {
-        const params = {
-            TableName: ipnsTable,
-            Item: record,
-        }
-
-        await dbbClient.put(params)
-        return 'Put Successful'
-    } catch (error) {
-        throw new DatabaseError()
+export default async (record: addIPNSRecord) => {
+  try {
+    const params = {
+      TableName: ipnsTable,
+      Item: record,
     }
+
+    await dbbClient.put(params)
+    return 'Put Successful'
+  } catch (error) {
+    throw new DatabaseError()
+  }
 }
