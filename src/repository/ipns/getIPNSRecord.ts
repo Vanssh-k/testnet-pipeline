3f66b1c33eb92c7604e1ae874280737a66e2e2f7
ravish <ravish1729@gmail.com>
2023-03-28 22:12:09 +0530

    get record

--- src/repository/ipns/getIPNSRecord.ts
+++ src/repository/ipns/getIPNSRecord.ts
@@ -2,21 +2,21 @@ import dbbClient from '../ddbClient'
 import { ipnsTable } from '../../controller/libs/constants'
 import DatabaseError from '../../errors/database-error'
 
-export default async (publicKey: any) => {
-    try {
-        const params = {
-            TableName: ipnsTable,
-            IndexName: 'publicKey-index',
-            KeyConditionExpression: 'publicKey = :p',
-            ExpressionAttributeValues: {
-                ':p': publicKey,
-            },
-        }
-
-        const records = await dbbClient.query(params)
-        const Items = records.Items?records.Items:[]
-        return Items
-    } catch (error) {
-        throw new DatabaseError()
+export default async (publicKey: string) => {
+  try {
+    const params = {
+      TableName: ipnsTable,
+      IndexName: 'publicKey-index',
+      KeyConditionExpression: 'publicKey = :p',
+      ExpressionAttributeValues: {
+        ':p': publicKey,
+      },
     }
+
+    const records = await dbbClient.query(params)
+    const Items = records.Items ? records.Items : []
+    return Items
+  } catch (error) {
+    throw new DatabaseError()
+  }
 }
