2c7b9587878d1e8b8c3c9baf9fe90c6357047bf1
ravish <ravish1729@gmail.com>
2023-03-28 23:46:28 +0530

    formatting

--- src/controller/governance/helper/proposalHelper.ts
+++ src/controller/governance/helper/proposalHelper.ts
@@ -1,21 +1,21 @@
 import { v4 } from 'uuid'
 import {
-    addProposal,
-    allProposals,
+  addProposal,
+  allProposals,
 } from '../../../repository/governance/proposals'
 
 export const createProposal = async (publicKey: string, proposal: string) => {
-    const timestamp = Date.now()
-    const _ = await addProposal({
-        id: v4().toString(),
-        publicKey,
-        proposal,
-        createdAt: timestamp,
-        updatedAt: timestamp,
-    })
-    return 'Success'
+  const timestamp = Date.now()
+  const _ = await addProposal({
+    id: v4().toString(),
+    publicKey,
+    proposal,
+    createdAt: timestamp,
+    updatedAt: timestamp,
+  })
+  return 'Success'
 }
 
 export const listProposals = async () => {
-    return await allProposals()
+  return await allProposals()
 }
--- src/controller/governance/index.ts
+++ src/controller/governance/index.ts
@@ -2,28 +2,28 @@ import { NextFunction, Request, Response } from 'express'
 import { createProposal, listProposals } from './helper/proposalHelper'
 
 export const create_proposal = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const { proposal, publicKey } = req.body
-        createProposal(publicKey, proposal)
-        res.status(200).json('Proposal Added')
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const { proposal, publicKey } = req.body
+    createProposal(publicKey, proposal)
+    res.status(200).json('Proposal Added')
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const list_proposals = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const proposals = await listProposals()
-        res.status(200).json(proposals)
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const proposals = await listProposals()
+    res.status(200).json(proposals)
+  } catch (error) {
+    next(error)
+  }
 }
