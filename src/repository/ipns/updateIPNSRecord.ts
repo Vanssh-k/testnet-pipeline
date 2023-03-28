5b2c59c6a780e31b6790237fc946da3289e16393
ravish <ravish1729@gmail.com>
2023-03-28 22:14:26 +0530

    update ipns record

--- src/repository/ipns/updateIPNSRecord.ts
+++ src/repository/ipns/updateIPNSRecord.ts
@@ -3,23 +3,22 @@ import { ipnsTable } from '../../controller/libs/constants'
 import DatabaseError from '../../errors/database-error'
 
 export default async (id: string, cid: string) => {
-    try {
-        const params = {
-            TableName: ipnsTable,
-            Key: {
-                ipnsName: id,
-            },
-            UpdateExpression: 'set cid = :c, lastUpdate = :u',
-            ExpressionAttributeValues: {
-                ':c': cid,
-                ':u': Date.now()
-            },
-        }
-
-        await dbbClient.update(params)
-        return 'Update Successful'
-    } catch (error) {
-        console.log(error)
-        throw new DatabaseError()
+  try {
+    const params = {
+      TableName: ipnsTable,
+      Key: {
+        ipnsName: id,
+      },
+      UpdateExpression: 'set cid = :c, lastUpdate = :u',
+      ExpressionAttributeValues: {
+        ':c': cid,
+        ':u': Date.now(),
+      },
     }
+
+    await dbbClient.update(params)
+    return 'Update Successful'
+  } catch (error) {
+    throw new DatabaseError()
+  }
 }
