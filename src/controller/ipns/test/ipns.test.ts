bdb84f8505c42b07d1b077d3a9922e18e648491c
ravish <ravish1729@gmail.com>
2023-03-28 21:46:20 +0530

    test cases added

---
+++ src/controller/ipns/test/ipns.test.ts
@@ -0,0 +1,42 @@
+import app from '../../../app'
+import supertest from 'supertest'
+import config from '../../../config'
+
+test('IPNS Test', async () => {
+  const apiKey = config.test_wallet7_api_key
+  
+  // Generate key
+  const ipns = JSON.parse((
+    await supertest(app)
+    .get('/api/ipns/generate_key')
+    .set('Authorization', `Bearer ${apiKey}`)
+  ).text)
+  const key = ipns.ipnsName
+  expect(typeof key).toBe('string')
+  
+  // Publish CID
+  const cid = 'QmWC9AkGa6vSbR4yizoJrFMfmZh4XjZXxvRDknk2LdJffc'
+  const publishCID = JSON.parse((
+    await supertest(app)
+    .get(`/api/ipns/publish_record?cid=${cid}&keyName=${key}`)
+    .set('Authorization', `Bearer ${apiKey}`)
+  ).text)
+  expect(typeof publishCID.Value).toBe('string')
+
+  // Get All CID
+  const allRecords = JSON.parse((
+    await supertest(app)
+    .get('/api/ipns/get_ipns_records')
+    .set('Authorization', `Bearer ${apiKey}`)
+  ).text)
+  expect(typeof allRecords[0]['ipnsName']).toBe('string')
+
+  // Remove Key
+  const removeKey = JSON.parse((
+    await supertest(app)
+    .delete(`/api/ipns/remove_key?keyName=${key}`)
+    .set('Authorization', `Bearer ${apiKey}`)
+  ).text)
+  expect(typeof removeKey.Keys[0]['Id']).toBe('string')
+
+}, 60000)
