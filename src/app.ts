db08b9da42a3c55d30b57238e5f540028c4fb335
ravish <ravish1729@gmail.com>
2023-03-29 13:50:00 +0530

    formatting

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Request, Response } from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-import errorHandler from './middlewares/error-handler'
 import AuthRouter from './routes/auth'
 import UserRouter from './routes/user'
-import TopUpRouter from './routes/topup'
 import IPNSRouter from './routes/ipns'
+import TopUpRouter from './routes/topup'
 import GovernanceRouter from './routes/governance'
 import LighthouseRouter from './routes/lighthouse'
+import express, { Request, Response } from 'express'
+import errorHandler from './middlewares/error-handler'
 
 const app = express()
 
@@ -18,13 +18,13 @@ app.use(morgan('dev'))
 app.use(cors())
 
 app.get('/api/health', (req: Request, res: Response) => {
-    res.status(200).send('OK')
+  res.status(200).send('OK')
 })
 
 app.use('/api/auth', AuthRouter)
 app.use('/api/user', UserRouter)
-app.use('/api/topup', TopUpRouter)
 app.use('/api/ipns', IPNSRouter)
+app.use('/api/topup', TopUpRouter)
 app.use('/api/governance', GovernanceRouter)
 app.use('/api/lighthouse', LighthouseRouter)
 
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import app from './app'
 import config from './config'
 
 app.listen(config.port, () => {
-    console.log('Server is running on port 8000')
+  console.log(`Server is running on port ${config.port}`)
 })
