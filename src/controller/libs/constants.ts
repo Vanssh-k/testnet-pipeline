38572750b522f6faab0538af6532ea232d20a2ef
ravish <ravish1729@gmail.com>
2023-03-29 22:41:55 +0530

    formatting

--- src/controller/libs/constants.ts
+++ src/controller/libs/constants.ts
@@ -1,4 +1,4 @@
-const userTable = 'Users'
+const userTable = 'user-record'
 const fileTable = 'files'
 const userAuthTable = 'user-auth'
 const gatewayTable = 'dedicatedGatewayRecords'
@@ -8,7 +8,7 @@ const ProposalTable = 'ProposalTable'
 const userTransactions = 'userTransactions'
 const subscriptionPlans = 'subscriptionPlans'
 const messageString =
-    'Please prove you are the owner of this wallet by signing this message, nonce='
+  'Please prove you are the owner of this wallet by signing this message, nonce='
 const freeDataLimitInBytes = 1073741824
 const filecoinDealRecords = 'filecoin-deal-records'
 const carBundleRecords = 'car-bundle-records'
@@ -16,66 +16,66 @@ const fileBundleRecords = 'file-bundle-records'
 const ipnsTable = 'user-ipns-records'
 const fileTableEncryption = 'file-table-encryption'
 const subscriptionPlanDetails = [
-    {
-        index: 0,
-        planName: 'Spark',
-        totalNumOfDeduction: 1,
-        nextDeductionInNumOfBlocks: 1200000,
-        amount: 10000000,
-        totalFilesPinned: 10000,
-        ipfsGBStorage: 30,
-        filecoinPermanentStorage: 'on demand',
-        dedicatedGateway: 1,
-        bandwidthInGB: 60,
-        requests: 100000,
-        encryptionAccessControl: 'mainnet',
-        imageResize: 'yes',
-        techSupportCall: 'no',
-        payInCrypto: 'yes',
-        filecoinDeals: 'yes',
-    },
-    {
-        index: 1,
-        planName: 'Blaze',
-        totalNumOfDeduction: 1,
-        nextDeductionInNumOfBlocks: 1200000,
-        amount: 50000000,
-        totalFilesPinned: 50000,
-        ipfsGBStorage: 150,
-        filecoinPermanentStorage: 'on demand',
-        dedicatedGateway: 3,
-        bandwidthInGB: 450,
-        requests: 1000000,
-        encryptionAccessControl: 'mainnet',
-        imageResize: 'yes',
-        techSupportCall: 'yes',
-        payInCrypto: 'yes',
-        filecoinDeals: 'yes',
-    },
+  {
+    index: 0,
+    planName: 'Spark',
+    totalNumOfDeduction: 1,
+    nextDeductionInNumOfBlocks: 1200000,
+    amount: 10000000,
+    totalFilesPinned: 10000,
+    ipfsGBStorage: 30,
+    filecoinPermanentStorage: 'on demand',
+    dedicatedGateway: 1,
+    bandwidthInGB: 60,
+    requests: 100000,
+    encryptionAccessControl: 'mainnet',
+    imageResize: 'yes',
+    techSupportCall: 'no',
+    payInCrypto: 'yes',
+    filecoinDeals: 'yes',
+  },
+  {
+    index: 1,
+    planName: 'Blaze',
+    totalNumOfDeduction: 1,
+    nextDeductionInNumOfBlocks: 1200000,
+    amount: 50000000,
+    totalFilesPinned: 50000,
+    ipfsGBStorage: 150,
+    filecoinPermanentStorage: 'on demand',
+    dedicatedGateway: 3,
+    bandwidthInGB: 450,
+    requests: 1000000,
+    encryptionAccessControl: 'mainnet',
+    imageResize: 'yes',
+    techSupportCall: 'yes',
+    payInCrypto: 'yes',
+    filecoinDeals: 'yes',
+  },
 ]
 const cacheClearTime = {
-    'day': 86400,
-    'week': 604800,
-    'month': 2628000
+  day: 86400,
+  week: 604800,
+  month: 2628000,
 }
 
 export {
-    userTable,
-    userAuthTable,
-    fileTable,
-    freeDataLimitInBytes,
-    gatewayTable,
-    migrationCIDs,
-    migrationRequestTable,
-    userTransactions,
-    ProposalTable,
-    subscriptionPlans,
-    messageString,
-    subscriptionPlanDetails,
-    filecoinDealRecords,
-    carBundleRecords,
-    fileBundleRecords,
-    fileTableEncryption,
-    ipnsTable,
-    cacheClearTime
+  userTable,
+  userAuthTable,
+  fileTable,
+  freeDataLimitInBytes,
+  gatewayTable,
+  migrationCIDs,
+  migrationRequestTable,
+  userTransactions,
+  ProposalTable,
+  subscriptionPlans,
+  messageString,
+  subscriptionPlanDetails,
+  filecoinDealRecords,
+  carBundleRecords,
+  fileBundleRecords,
+  fileTableEncryption,
+  ipnsTable,
+  cacheClearTime,
 }
