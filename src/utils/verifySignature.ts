98d5a720efb5e031fcb0266adc0005035f113b74
ravish <ravish1729@gmail.com>
2023-03-29 22:42:26 +0530

    ethers fixes

--- src/utils/verifySignature.ts
+++ src/utils/verifySignature.ts
@@ -3,30 +3,29 @@ import { ethers } from 'ethers'
 import nacl from 'tweetnacl'
 
 export default (
-    usersPublicKey: string,
-    originalMessage: string,
-    signedMessage: string,
-    network:string
+  usersPublicKey: string,
+  originalMessage: string,
+  signedMessage: string,
+  network: string
 ) => {
-    try {
-        if (network === 'evm') {
-            const sig = ethers.utils.splitSignature(signedMessage)
-            const publicKeyToVerify = ethers.utils
-                .verifyMessage(originalMessage, sig)
-                .toLowerCase()
-            if (usersPublicKey.toLowerCase() === publicKeyToVerify) {
-                return true
-            }
-            return false
-        }
-        const verified = nacl.sign.detached.verify(
-            new TextEncoder().encode(originalMessage),
-            bs58.decode(signedMessage),
-            bs58.decode(usersPublicKey)
-        )
-        // check response data type of verified
-        return verified
-    } catch {
-        return false
+  try {
+    if (network === 'evm') {
+      const publicKeyToVerify = ethers
+        .verifyMessage(originalMessage, signedMessage)
+        .toLowerCase()
+      if (usersPublicKey.toLowerCase() === publicKeyToVerify) {
+        return true
+      }
+      return false
     }
+    const verified = nacl.sign.detached.verify(
+      new TextEncoder().encode(originalMessage),
+      bs58.decode(signedMessage),
+      bs58.decode(usersPublicKey)
+    )
+    // check response data type of verified
+    return verified
+  } catch {
+    return false
+  }
 }
