d1fd38c67b131e6dccf712164935ccc6dc06b50d
ravish <ravish1729@gmail.com>
2023-03-31 01:13:15 +0530

    plans shifted

--- src/controller/libs/constants.ts
+++ src/controller/libs/constants.ts
@@ -15,44 +15,6 @@ const carBundleRecords = 'car-bundle-records'
 const fileBundleRecords = 'file-bundle-records'
 const ipnsTable = 'user-ipns-records'
 const fileTableEncryption = 'file-table-encryption'
-const subscriptionPlanDetails = [
-  {
-    index: 0,
-    planName: 'Spark',
-    totalNumOfDeduction: 1,
-    nextDeductionInNumOfBlocks: 1200000,
-    amount: 10000000,
-    totalFilesPinned: 10000,
-    ipfsGBStorage: 30,
-    filecoinPermanentStorage: 'on demand',
-    dedicatedGateway: 1,
-    bandwidthInGB: 60,
-    requests: 100000,
-    encryptionAccessControl: 'mainnet',
-    imageResize: 'yes',
-    techSupportCall: 'no',
-    payInCrypto: 'yes',
-    filecoinDeals: 'yes',
-  },
-  {
-    index: 1,
-    planName: 'Blaze',
-    totalNumOfDeduction: 1,
-    nextDeductionInNumOfBlocks: 1200000,
-    amount: 50000000,
-    totalFilesPinned: 50000,
-    ipfsGBStorage: 150,
-    filecoinPermanentStorage: 'on demand',
-    dedicatedGateway: 3,
-    bandwidthInGB: 450,
-    requests: 1000000,
-    encryptionAccessControl: 'mainnet',
-    imageResize: 'yes',
-    techSupportCall: 'yes',
-    payInCrypto: 'yes',
-    filecoinDeals: 'yes',
-  },
-]
 const cacheClearTime = {
   day: 86400,
   week: 604800,
@@ -71,7 +33,6 @@ export {
   ProposalTable,
   subscriptionPlans,
   messageString,
-  subscriptionPlanDetails,
   filecoinDealRecords,
   carBundleRecords,
   fileBundleRecords,
