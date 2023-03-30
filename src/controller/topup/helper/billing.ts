9f5178d9e297f2c809124e00fbe21d13c700cfc6
ravish <ravish1729@gmail.com>
2023-03-31 01:13:45 +0530

    moved to topup

---
+++ src/controller/topup/helper/billing.ts
@@ -0,0 +1,22 @@
+import { ethers } from 'ethers'
+import envConfig from '../../../config'
+import billingABI from '../../../contract_abi/billing'
+
+const mumbaiProvider = new ethers.JsonRpcProvider(envConfig.polygon_rpc)
+const mumbaiBillingContract = new ethers.Contract(
+  envConfig.lighthouse_billing_address,
+  billingABI,
+  mumbaiProvider
+)
+
+const getSubscriptionStatus = async (publicKey: string) => {
+  const data = await mumbaiBillingContract.subscriptionStatus(publicKey)
+  return { status: data[0], subscriptionId: data[1] }
+}
+
+const getPurchasablePlans = async () => {
+  const data = await mumbaiBillingContract.getActivePlans()
+  return { activePurchasablePlans: data }
+}
+
+export { getSubscriptionStatus, getPurchasablePlans }
