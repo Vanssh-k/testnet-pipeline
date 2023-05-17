ef2b3058e7348e19b985352ac9639d31f92b90cf
ravish <ravish1729@gmail.com>
2023-05-18 03:37:47 +0530

    test cases moved to test directory user cont

--- src/controller/user/test/index.test.ts
+++ src/controller/user/test/index.test.ts
@@ -1,6 +1,6 @@
-import app from '../../app'
+import app from '../../../app'
 import supertest from 'supertest'
-import config from '../../config'
+import config from '../../../config'
 
 // get_uploads
 describe('userHelper', () => {
