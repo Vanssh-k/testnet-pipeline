22a2b8d6ffe772a1b1bb201c8936e156dbdb6f2a
ravish <ravish1729@gmail.com>
2023-07-20 10:58:09 +0530

    testnet operations

---
+++ src/repository/filecoin/testnet/podsiRecordTestnet.ts
@@ -0,0 +1,21 @@
+import dbbClient from '../../db/ddbClient'
+import DatabaseError from '../../../errors/database-error'
+
+export default async (pieceCID: string) => {
+  try {
+    const params = {
+      TableName: 'testnet-file-podsi',
+      IndexName: 'pieceCID-index',
+      KeyConditionExpression: 'pieceCID = :p',
+      ExpressionAttributeValues: {
+        ':p': pieceCID,
+      },
+    }
+
+    const record = await dbbClient.query(params)
+    return record.Items ?? []
+  } catch (error) {
+    /* istanbul ignore next */
+    throw new DatabaseError()
+  }
+}
