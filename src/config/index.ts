b39fb435498a957e783f0e90b98225c81f7043b7
ravish <ravish1729@gmail.com>
2023-08-18 09:18:38 +0530

    ipns path update

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ const baseConfig = {
   isTest: env === 'testing',
   logPath: 'logs',
   lighthouse_ipfs_node: 'https://node.lighthouse.storage',
-  lighthouse_ipns_node: 'http://10.0.6.68',
+  lighthouse_ipns_node: 'http://10.0.9.53',
   est_api_key: process.env.EST_API_KEY ?? '',
   covalent_api_key: process.env.COVALENT_API_KEY ?? '',
   aws_access_key_id: process.env.AWS_ACCESS_KEY_ID ?? '',
