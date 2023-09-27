6a7471c76bc034c6ad57d699f8a5a332f7abb04b
xlassix <awhy14539@gmail.com>
2023-09-27 07:26:18 +0100

    change

--- src/controller/user/helper/userHelper.ts
+++ src/controller/user/helper/userHelper.ts
@@ -87,30 +87,32 @@ export const updateDataUsage = async (
   return 'Success'
 }
 
-export const createTagHelper = async (tag: string, cid: string, publicKey: string) => {
+export const createTagHelper = async (
+  tag: string,
+  cid: string,
+  publicKey: string
+) => {
   const saveResponse = await createTag({
-    id: publicKey+'-'+tag,
+    id: publicKey + '-' + tag,
     tag: tag,
     cid: cid,
     publicKey: publicKey,
-    lastUpdate: Date.now()
+    lastUpdate: Date.now(),
   })
   return saveResponse
 }
 
 export const getTagDetailsHelper = async (tag: string, publicKey: string) => {
-  const tagDetails = await getTagData(publicKey+'-'+tag)
+  const tagDetails = await getTagData(publicKey + '-' + tag)
   return tagDetails
 }
-
 
 export const getAllTagsHelper = async (publicKey: string) => {
   const tags = await getAllTags(publicKey)
   return tags
 }
 
-
 export const removeTagHelper = async (tag: string, publicKey: string) => {
-  const tagDetails = await removeTag(publicKey+'-'+tag)
+  const tagDetails = await removeTag(publicKey + '-' + tag)
   return 'Success'
 }
