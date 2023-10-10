ef72d37efb641501e8c5049f4e01cdd42a17f585
ravish <ravish1729@gmail.com>
2023-10-11 02:56:11 +0530

    pinning added

--- src/controller/lighthouse/helper/migrationHelper.ts
+++ src/controller/lighthouse/helper/migrationHelper.ts
@@ -2,8 +2,67 @@ import axios from 'axios'
 import { v4 } from 'uuid'
 import verifyCID from '../../../utils/verifyCID'
 import { DatabaseError, BadRequestError } from '../../../errors'
+import processDealParam from './processDealParameters'
+import testnetDealParams from '../../../repository/filecoin/testnet/testnetDealParams'
 import addMigrationCIDs from '../../../repository/migration/addMigrationCIDs'
 import createMigrationRequest from '../../../repository/migration/createMigrationRequest'
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const addCIDToRAASTestnet = async(cid: string, fileID: string) =>{
+  await delay(30000) // after 5 min
+  const __ = await axios.get(
+    `https://calibration.lighthouse.storage/api/deal/add_cid?cid=${cid}&fileId=${fileID}`,
+  );
+}
+
+export const pinCID = async (record: any, cid: string, fileName: string, raas: any) => {
+  // Verify CID's
+  if (!verifyCID(cid)) {
+    throw new BadRequestError(`Invalid CID`)
+  }
+
+  // Save Migration Request
+  const timestamp = Date.now()
+  const requestID = v4().toString()
+  const saveRequest = await createMigrationRequest({
+    id: requestID,
+    publicKey: record.publicKey,
+    totalCID: 1,
+    migrationStatus: 'queued',
+    enterprise: 'lighthouse',
+    createdAt: timestamp,
+    lastUpdate: timestamp,
+  })
+
+  const id = v4().toString()
+  const saveCID = await addMigrationCIDs({
+    id: id,
+    cid: cid,
+    requestID,
+    fileName: fileName,
+    fileSizeInBytes: '',
+    userDataUpdated: false,
+    txHash: '',
+    cidStatus: 'queued',
+    deal: '',
+    lastUpdate: timestamp,
+  })
+
+  if(raas && raas["network"] === "calibration") {
+    // handle deal parameters
+    const dealParam = processDealParam(raas, id)
+    if(dealParam) {
+      await testnetDealParams(dealParam)
+    }
+    const addCIDToTestnet = addCIDToRAASTestnet(cid, id)
+  }
+
+  const startMigration = axios.get(
+    `http://43.205.115.104/api?requestId=${requestID}`
+  )
+  
+  return requestID
+}
 
 export const migrationRequest = async (record: any, bodyData: string) => {
   // Get CID, filename array
