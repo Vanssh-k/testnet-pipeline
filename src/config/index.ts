b76a66ce3206777c4b9e0c97602ecc3ce5081a77
ravish <ravish1729@gmail.com>
2023-11-15 02:30:16 +0530

    datadog credentials

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,8 +9,9 @@ const baseConfig = {
   isDev: env === 'development',
   isTest: env === 'testing',
   logPath: 'logs',
+  service: 'backend',
   lighthouse_ipfs_node: 'https://node.lighthouse.storage',
-  lighthouse_auth_enc_node: "https://encryption.lighthouse.storage",
+  lighthouse_auth_enc_node: 'https://encryption.lighthouse.storage',
   lighthouse_encryption_nodes: [
     'https://encryption.lighthouse.storage/api/auth-message/1',
     'https://encryption.lighthouse.storage/api/auth-message/2',
@@ -18,7 +19,9 @@ const baseConfig = {
     'https://encryption.lighthouse.storage/api/auth-message/4',
     'https://encryption.lighthouse.storage/api/auth-message/5',
   ],
-  lighthouse_encryption_auth_keys: JSON.parse(process.env.LIGHTHOUSE_ENC_MESSAGE_TOKENS ?? '[]'),
+  lighthouse_encryption_auth_keys: JSON.parse(
+    process.env.LIGHTHOUSE_ENC_MESSAGE_TOKENS ?? '[]'
+  ),
   lighthouse_ipns_node: 'http://10.0.9.53',
   est_api_key: process.env.EST_API_KEY ?? '',
   covalent_api_key: process.env.COVALENT_API_KEY ?? '',
@@ -26,6 +29,8 @@ const baseConfig = {
   aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY ?? '',
   log_storage_bucket: 'lighthouse-logs-store',
   cloudflare_key: process.env.CLOUDFLARE_KEY ?? '',
+  data_dog_key: process.env.DATADOG_KEY ?? '',
+  data_dog_host: process.env.DATADOG_HOST ?? '',
   transaction_route_token: process.env.TRANSACTION_ROUTE_TOKEN ?? '',
   polygon_rpc: process.env.POLYGON_RPC ?? '',
   migration_test_access_token: process.env.MIGRATION_TEST_ACCESS_TOKEN ?? '',
@@ -35,12 +40,24 @@ const baseConfig = {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET ?? '',
   jwt_secret: process.env.JWT_SECRET ?? '',
   twitter_api_key: process.env.TWITTER_API_KEY ?? '',
-  test_wallet1_private_key: process.env.TEST_WALLET1_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de81',
-  test_wallet2_private_key: process.env.TEST_WALLET2_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de82',
-  test_wallet3_private_key: process.env.TEST_WALLET3_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de83',
-  test_wallet4_private_key: process.env.TEST_WALLET4_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de84',
-  test_wallet5_private_key: process.env.TEST_WALLET5_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de85',
-  test_wallet6_private_key: process.env.TEST_WALLET6_PRIVATE_KEY ?? '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de86',
+  test_wallet1_private_key:
+    process.env.TEST_WALLET1_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de81',
+  test_wallet2_private_key:
+    process.env.TEST_WALLET2_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de82',
+  test_wallet3_private_key:
+    process.env.TEST_WALLET3_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de83',
+  test_wallet4_private_key:
+    process.env.TEST_WALLET4_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de84',
+  test_wallet5_private_key:
+    process.env.TEST_WALLET5_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de85',
+  test_wallet6_private_key:
+    process.env.TEST_WALLET6_PRIVATE_KEY ??
+    '8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de86',
   test_wallet7_api_key: process.env.TEST_WALLET7_API_KEY ?? '0x02',
   lighthouse_billing_address: process.env.LIGHTHOUSE_BILLING_ADDRESS ?? '0x02',
   migration_ocean_access_token:
@@ -86,6 +103,8 @@ const envVarsSchema = Joi.object({
     'any.required': `'TWITTER_API_KEY IS MISSING'`,
   }),
 
+  data_dog_key: Joi.string(),
+  data_dog_host: Joi.string(),
   lighthouse_public_node_token: Joi.string(),
   migration_ocean_access_token: Joi.string(),
   lighthouse_billing_address: Joi.string(),
