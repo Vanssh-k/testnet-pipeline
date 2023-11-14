3acfe4a13120b4133ab09416f7c41ce34f87b8df
ravish <ravish1729@gmail.com>
2023-11-14 11:49:37 +0530

    refactor

--- src/controller/authentication/test/APIKey.test.ts
+++ src/controller/authentication/test/APIKey.test.ts
@@ -2,115 +2,82 @@ import { ethers } from 'ethers'
 import app from '../../../app'
 import supertest from 'supertest'
 import config from '../../../config'
-import { getMessage } from '../helper/authHelper'
-import { sign } from 'crypto'
-import { getEncJWT, sendMessageToEnc, useEncRefreshToken } from '../helper/encryption'
 
-describe("APIkey Test", () => {
+describe('APIkey Test', () => {
   const signer = new ethers.Wallet(config.test_wallet1_private_key)
-  test('Api Key Get and Verify: POST /get_api_key, GET /verify_api_key', async () => {
-    const verificationMessage = JSON.parse(
-      (
-        await supertest(app).get(
-          '/api/auth/get_message?publicKey=0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1'
-        )
-      ).text
-    )
-    expect(typeof verificationMessage).toBe('string')
+  const publicKey = '0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1'
+  const testUrl = '/api/auth'
 
+  const getResponseText = async (request) => JSON.parse((await request).text)
+  const verifyType = (data, type) => expect(typeof data).toBe(type)
+
+  test('Api Key Get and Verify: POST /get_api_key, GET /verify_api_key', async () => {
+    const verificationMessage = await getResponseText(
+      supertest(app).get(`${testUrl}/get_message?publicKey=${publicKey}`)
+    )
+    verifyType(verificationMessage, 'string')
 
     const signedMessage = await signer.signMessage(verificationMessage)
-    const data = {
-      publicKey: '0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1',
-      signedMessage,
-    }
+    const data = { publicKey, signedMessage }
 
-    const apiKey = JSON.parse(
-      (await supertest(app).post('/api/auth/get_api_key').send(data)).text
+    const apiKey = await getResponseText(
+      supertest(app).post(`${testUrl}/get_api_key`).send(data)
     )
-
-    expect(typeof apiKey).toBe('string')
+    verifyType(apiKey, 'string')
 
     // Verify API Key
-    const verifyResponse = JSON.parse(
-      (
-        await supertest(app)
-          .get('/api/auth/verify_api_key')
-          .set('Authorization', `Bearer ${apiKey}`)
-      ).text
+    const verifyResponse = await getResponseText(
+      supertest(app)
+        .get(`${testUrl}/verify_api_key`)
+        .set('Authorization', `Bearer ${apiKey}`)
     )
-    expect(typeof verifyResponse.publicKey).toBe('string')
+    verifyType(verifyResponse.publicKey, 'string')
 
     // Get all keys
-    const allKeys = JSON.parse(
-      (
-        await supertest(app)
-          .get('/api/auth/get_user_keys')
-          .set('Authorization', `Bearer ${apiKey}`)
-      ).text
+    const allKeys = await getResponseText(
+      supertest(app)
+        .get(`${testUrl}/get_user_keys`)
+        .set('Authorization', `Bearer ${apiKey}`)
     )
-    expect(typeof allKeys[0]['id']).toBe('string')
+    verifyType(allKeys[0]['id'], 'string')
 
     // Revoke API Key Forbidden
     await supertest(app)
-      .delete(`/api/auth/remove_api_key?keyId=${allKeys[0]['id']}`)
+      .delete(`${testUrl}/remove_api_key?keyId=${allKeys[0]['id']}`)
       .set('Authorization', `Bearer ${config.test_wallet7_api_key}`)
       .expect(403)
 
     // Revoke API Key
-    const revokeResponse = JSON.parse(
-      (
-        await supertest(app)
-          .delete(`/api/auth/remove_api_key?keyId=${allKeys[0]['id']}`)
-          .set('Authorization', `Bearer ${apiKey}`)
-      ).text
+    const revokeResponse = await getResponseText(
+      supertest(app)
+        .delete(`${testUrl}/remove_api_key?keyId=${allKeys[0]['id']}`)
+        .set('Authorization', `Bearer ${apiKey}`)
     )
-    expect(typeof revokeResponse.data).toBe('string')
+    verifyType(revokeResponse.data, 'string')
   }, 10000)
 
   test('Api Key test on old key: GET /verify_api_key', async () => {
-    const data = JSON.parse(
-      (
-        await supertest(app)
-          .get('/api/auth/verify_api_key')
-          .set('Authorization', `Bearer ${config.test_wallet7_api_key}`)
-      ).text
+    const data = await getResponseText(
+      supertest(app)
+        .get(`${testUrl}/verify_api_key`)
+        .set('Authorization', `Bearer ${config.test_wallet7_api_key}`)
     )
-    expect(typeof data.publicKey).toBe('string')
+    verifyType(data.publicKey, 'string')
   }, 10000)
 
   test('Api Key Record Not Authorized: POST /get_api_key', async () => {
-    const data = {
-      publicKey: '0xEaF4E24ffC1A2f53c07839a74966A6611b8Cb8A1',
-      signedMessage: 'signedMessage',
-    }
-
-    await supertest(app).post('/api/auth/get_api_key').send(data).expect(401)
+    const data = { publicKey, signedMessage: 'signedMessage' }
+    await supertest(app).post(`${testUrl}/get_api_key`).send(data).expect(401)
   }, 10000)
 
   test('Verify API Key Record Not Found: GET /verify_api_key', async () => {
     await supertest(app)
-      .get('/api/auth/verify_api_key')
+      .get(`${testUrl}/verify_api_key`)
       .set('Authorization', 'Bearer ' + '937b68b8-3768-45d1-950b-30c3836785d5')
       .expect(401)
   }, 10000)
 
   test('Verify API Key Bad Request: GET /verify_api_key', async () => {
-    await supertest(app).get('/api/auth/verify_api_key').expect(401)
+    await supertest(app).get(`${testUrl}/verify_api_key`).expect(401)
   }, 10000)
-  test('Encryption ', async () => {
-    let test_message = "message data"
-    const data = await sendMessageToEnc(signer.address, test_message)
-    const { encryptionRefreshToken, encryptionToken } = await getEncJWT(signer.address, await signer.signMessage(test_message))
-    expect(typeof encryptionToken).toBe("string")
-    expect(typeof encryptionRefreshToken).toBe("string")
-    expect(encryptionToken?.length).toBeGreaterThan(100)
-
-
-    const tokens = await useEncRefreshToken(signer.address, encryptionRefreshToken)
-    expect(typeof tokens.encryptionToken).toBe("string")
-    expect(typeof tokens.encryptionRefreshToken).toBe("string")
-  }, 10000)
-
-
 })
--- src/controller/authentication/test/getMessage.test.ts
+++ src/controller/authentication/test/getMessage.test.ts
@@ -1,41 +1,63 @@
 import app from '../../../app'
 import supertest from 'supertest'
-import { sendMessageToEnc } from '../helper/encryption'
+
+const testGetMessageWithEnc = async (
+  publicKey: string,
+  expectedStatusCode: number,
+  expectedType: string
+) => {
+  const response = await supertest(app).get(
+    `/api/auth/get_message?publicKey=${publicKey}&encryption=true`
+  )
+  const message = JSON.parse(response.text)
+  expect(response.status).toBe(expectedStatusCode)
+  expect(typeof message).toBe(expectedType)
+}
+
+const testGetMessage = async (
+  publicKey: string,
+  expectedStatusCode: number,
+  expectedType: string
+) => {
+  const response = await supertest(app).get(
+    `/api/auth/get_message?publicKey=${publicKey}`
+  )
+  const message = JSON.parse(response.text)
+
+  expect(response.status).toBe(expectedStatusCode)
+  if (expectedStatusCode !== 400) {
+    expect(typeof message).toBe(expectedType)
+  }
+}
 
 test('EVM Message: wrong publicKey: GET /get_message', async () => {
-  await supertest(app)
-    .get(
-      '/api/auth/get_message?publicKey=0xA3C960B3BA29367ecBCAf1430452C6cd7516F58'
-    )
-    .expect(400)
+  await testGetMessage(
+    '0xA3C960B3BA29367ecBCAf1430452C6cd7516F58',
+    400,
+    'string'
+  )
 }, 10000)
 
 test('EVM Message: GET /get_message', async () => {
-  const message = JSON.parse(
-    (
-      await supertest(app).get(
-        '/api/auth/get_message?publicKey=0xA3C960B3BA29367ecBCAf1430452C6cd7516F588'
-      )
-    ).text
+  await testGetMessage(
+    '0xA3C960B3BA29367ecBCAf1430452C6cd7516F588',
+    200,
+    'string'
   )
-  expect(typeof message).toBe('string')
 }, 10000)
 
 test('Solana Message: GET /get_message', async () => {
-  const message = JSON.parse(
-    (
-      await supertest(app).get(
-        '/api/auth/get_message?publicKey=J7wvyu9x2vzAssgkNKi4hHdfuJaJAHZV9RxXPrsbkLEK'
-      )
-    ).text
+  await testGetMessage(
+    'J7wvyu9x2vzAssgkNKi4hHdfuJaJAHZV9RxXPrsbkLEK',
+    200,
+    'string'
   )
-  expect(typeof message).toBe('string')
 }, 10000)
 
 test('Message to enc node as well', async () => {
-  const data = await sendMessageToEnc(
+  await testGetMessageWithEnc(
     '0xA3C960B3BA29367ecBCAf1430452C6cd7516F588',
-    'message data'
+    200,
+    'string'
   )
-  expect(typeof data?.[0]['message']).toBe('string')
 }, 10000)
