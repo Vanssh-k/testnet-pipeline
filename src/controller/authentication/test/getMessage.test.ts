d4c52cfd1793551da71448de36bc1799ef922cde
ravish <ravish1729@gmail.com>
2023-11-14 08:13:03 +0530

    added test cases

--- src/controller/authentication/test/getMessage.test.ts
+++ src/controller/authentication/test/getMessage.test.ts
@@ -1,5 +1,14 @@
 import app from '../../../app'
 import supertest from 'supertest'
+import { sendMessageToEnc } from '../helper/encryption'
+
+test('EVM Message: wrong publicKey: GET /get_message', async () => {
+  await supertest(app)
+    .get(
+      '/api/auth/get_message?publicKey=0xA3C960B3BA29367ecBCAf1430452C6cd7516F58'
+    )
+    .expect(400)
+}, 10000)
 
 test('EVM Message: GET /get_message', async () => {
   const message = JSON.parse(
@@ -21,4 +30,12 @@ test('Solana Message: GET /get_message', async () => {
     ).text
   )
   expect(typeof message).toBe('string')
+}, 10000)
+
+test('Message to enc node as well', async () => {
+  const data = await sendMessageToEnc(
+    '0xA3C960B3BA29367ecBCAf1430452C6cd7516F588',
+    'message data'
+  )
+  expect(typeof data?.[0]['message']).toBe('string')
 }, 10000)
