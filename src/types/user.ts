94843d1053109b2a50da66962483bf613a2a49a6
ravish <ravish1729@gmail.com>
2023-11-14 08:14:10 +0530

    created types

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export interface IUserDetails {
+  publicKey: string
+  message: number
+  dataLimit: number
+  dataUsed: number
+  fileCount: number
+  faucet: any
+  network: string
+  createdAt: number
+  updatedAt: number
+}
