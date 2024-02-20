10e8aaabcb71acfe45e65ef2cde4addfc704b779
ravish1729 <ravish1729@gmail.com>
2024-02-21 02:51:05 +0530

    verify web3-auth email id

--- src/controller/user/index.ts
+++ src/controller/user/index.ts
@@ -12,6 +12,8 @@ import {
   generateTokenAndSendMail,
   verifyEmailToken,
 } from './helper/verifyEmail'
+import * as jose from 'jose'
+import updateEmail from '../../repository/user/updateEmail'
 
 export const get_uploads = async (
   req: Request,
@@ -175,6 +177,37 @@ export const verify_email_token = async (
       (req.query.verification_token as string).trim()
     )
     res.status(200).send(status)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const verify_web3auth_token = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const idToken = req.headers.authorization?.split(' ')[1]
+    const app_pub_key = req.body.appPubKey
+    const jwks = jose.createRemoteJWKSet(
+      new URL('https://api-auth.web3auth.io/jwks')
+    )
+    const jwtDecoded = await jose.jwtVerify(idToken as string, jwks, {
+      algorithms: ['ES256'],
+    })
+
+    if (
+      (jwtDecoded.payload as any).wallets[0].public_key.toLowerCase() ===
+      app_pub_key.toLowerCase()
+    ) {
+      // Verified
+      updateEmail(app_pub_key.toLowerCase(), (jwtDecoded.payload as any).email)
+
+      res.status(200).json({ name: 'Verification Successful' })
+    } else {
+      res.status(400).json({ name: 'Verification Failed' })
+    }
   } catch (error) {
     next(error)
   }
