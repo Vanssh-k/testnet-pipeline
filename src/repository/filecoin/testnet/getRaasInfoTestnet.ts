c7055761c02e71d4c8f9e2f992628d62c507f462
lordforever <shashanktrivedi1917@gmail.com>
2024-03-27 11:29:06 +0530

    scan to query

--- src/repository/filecoin/testnet/getRaasInfoTestnet.ts
+++ src/repository/filecoin/testnet/getRaasInfoTestnet.ts
@@ -5,15 +5,15 @@ export default async (cid: string) => {
   try {
     const params = {
       TableName: TestnetTableName.RAAS_TABLE,
-      // IndexName: 'cid-index',
-      FilterExpression: 'cid = :c',
-      // KeyConditionExpression: 'cid = :c',
+      IndexName: 'cid-index',
+      // FilterExpression: 'cid = :c',
+      KeyConditionExpression: 'cid = :c',
       ExpressionAttributeValues: {
         ':c': cid,
       },
     }
-    const record = await dbbClient.scan(params)
-    // const record = await dbbClient.query(params)
+    // const record = await dbbClient.scan(params)
+    const record = await dbbClient.query(params)
     return record.Items ?? []
   } catch (error) {
     /* istanbul ignore next */
