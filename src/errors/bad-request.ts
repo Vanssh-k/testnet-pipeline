946ec9847864c5348cbfc0f005bbcc8c456660ed
ravish1729 <ravish1729@gmail.com>
2024-04-10 01:49:43 +0530

    removed

--- src/errors/bad-request.ts
+++
@@ -1,7 +0,0 @@
-import CustomError from './custom-error'
-
-export default class BadRequestError extends CustomError {
-  constructor(message: string = 'Bad Request') {
-    super('Bad Request', 400, message)
-  }
-}
