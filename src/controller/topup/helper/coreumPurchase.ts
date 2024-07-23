0194b461aefa53b3c097b02510cb313f1c7cdf74
aakash-taneja <aakashtaneja38@gmail.com>
2024-07-23 19:46:58 +0530

    datacapPurchase fixed

--- src/controller/topup/helper/coreumPurchase.ts
+++ src/controller/topup/helper/coreumPurchase.ts
@@ -43,7 +43,7 @@ const validatePayment = async (req: Request) => {
       createdAt: Date.now(),
     })
     const paymentPlan = paymentPlans.find((plan) => plan.index === Number(req.body.subscriptionId))
-    const dataCapPurchased = paymentPlan ? paymentPlan.storageInGB : 0
+    const dataCapPurchased = (paymentPlan ? paymentPlan.storageInGB : 0) * 1073741824
 
     return dataCapPurchased
   }
