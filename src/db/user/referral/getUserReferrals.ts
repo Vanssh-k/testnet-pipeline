d56eb4a563baafe2636f2b623ab320e6fbf8cb3f
ravish1729 <ravish1729@gmail.com>
2025-02-24 01:49:20 +0530

    type correction

--- src/db/user/referral/getUserReferrals.ts
+++ src/db/user/referral/getUserReferrals.ts
@@ -4,7 +4,7 @@ import { Referral } from '../../../types/user.js'
 import { referralTable } from '../../../config/constants.js'
 import CustomError from '../../../middlewares/error/customError.js'
 
-export default async (usersPublicKey: string): Promise<Referral[] | null> => {
+export default async (usersPublicKey: string): Promise<Referral[] | undefined> => {
   try {
     const params = {
       TableName: referralTable,
@@ -16,7 +16,7 @@ export default async (usersPublicKey: string): Promise<Referral[] | null> => {
     }
 
     const record = await dbbClient.query(params)
-    return (record.Items as Referral[]) ?? null
+    return (record.Items as Referral[]) ?? undefined
   } catch (error: any) {
     logger.error('Referral Detail Fetch Error: ' + error.message)
     throw new CustomError(500, `Internal Server Error.`)
